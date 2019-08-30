d966b38e44da53e29d9a7479f74f8bd861fd9600
Kristofer Selbekk <selbeezy@gmail.com>
2019-08-30 21:42:52 +0200

    chore: add sass support to the docs site
    affects: @entur/designsystem-docs

--- packages/docs/gatsby-config.js
+++ packages/docs/gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     'gatsby-theme-docz',
     `gatsby-transformer-sharp`,
+    'gatsby-plugin-sass',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
