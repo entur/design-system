aacd5df7cbb2b486705b9a4145db51ba3904de81
Kristofer Selbekk <selbeezy@gmail.com>
2019-09-01 12:11:19 +0200

    docs(docs): add some styles and css variables to the ColorList component
    affects: @entur/designsystem-docs
    
    The css variables aren't quite right though, the misc colors and the numbers are not correct. Will
    have to dig deeper to figure out what we want here.

--- packages/docs/src/components/ColorList.js
+++ packages/docs/src/components/ColorList.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './ColorList.scss';
 
+const camelCaseToKebabCase = str =>
+  str.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
+
 export default function ColorList(props) {
   return (
     <ul className="color-list">
       {Object.entries(props.colors).map(([name, hex]) => (
         <li className="color-list__item" style={{ backgroundColor: hex }}>
-          {name}: {hex}
+          <div className="color-list__detail">
+            {name}: {hex}
+            <br />
+            --{props.category}-{camelCaseToKebabCase(name)}
+          </div>
         </li>
       ))}
     </ul>
