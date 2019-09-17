fc70a0daad747108190ecf94fce8f5703a132bf7
Kristofer Selbekk <selbeezy@gmail.com>
2019-09-17 14:11:14 +0200

    chore(docs): use typography package in docs
    affects: @entur/designsystem-docs

--- docs/src/gatsby-theme-docz/index.tsx
+++ docs/src/gatsby-theme-docz/index.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import { theme, ComponentsProvider } from 'docz';
-import './index.scss';
-import '@entur/fonts/index.css';
+import * as typography from '@entur/typography';
 import Menu from './UI/Menu';
 
-const compomentMap = {};
+import './index.scss';
 
-const Theme = ({ children }) => {
-  return (
-    <div>
-      <ComponentsProvider components={compomentMap}>
-        <div className="components-provider-wrapper">
-          <Menu />
-          <div className="content-wrapper">
-            <div className="content-container">{children}</div>
-          </div>
-        </div>
-      </ComponentsProvider>
-    </div>
-  );
+const componentMap = {
+  h1: typography.Heading1,
+  h2: typography.Heading2,
+  h3: typography.Heading3,
+  h4: typography.Heading4,
+  h5: typography.Heading5,
+  h6: typography.Heading6,
+  p: typography.Paragraph,
+  a: typography.Link,
 };
 
-export default theme()(Theme);
+export default theme({})(({ children }) => (
+  <div>
+    <div className="components-provider-wrapper">
+      <Menu />
+      <div className="content-wrapper">
+        <ComponentsProvider components={componentMap}>
+          <div className="content-container">{children}</div>
+        </ComponentsProvider>
+      </div>
+    </div>
+  </div>
+));
