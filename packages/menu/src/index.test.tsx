7ce57a5e2c30358b3c06ef82263436b12a6b27e6
Kristofer Selbekk <selbeezy@gmail.com>
2019-09-20 12:19:46 +0200

    test(menu): add test coverage for the menu package
    affects: @entur/menu

--- packages/menu/src/index.test.tsx
+++ packages/menu/src/index.test.tsx
@@ -1,6 +1,111 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import { Menu, MenuItem } from '.';
 
-test('still need some tests', () => {
-  // still todo
+test('renders a single-level menu without issues', () => {
+  const { getByText } = render(
+    <Menu>
+      <MenuItem href="#first">First item</MenuItem>
+      <MenuItem href="#second">Second item</MenuItem>
+    </Menu>,
+  );
+
+  expect(getByText('First item')).toBeInTheDocument();
+  expect(getByText('Second item')).toBeInTheDocument();
+
+  expect(getByText('First item')).toHaveAttribute('href', '#first');
+  expect(getByText('Second item')).toHaveAttribute('href', '#second');
+
+  expect(getByText('First item').nodeName).toBe('A');
+});
+
+test('renders a multi-level menu', () => {
+  const { getByText, queryByText } = render(
+    <Menu>
+      <MenuItem href="#">Top level</MenuItem>
+      <MenuItem>
+        Sub-menu trigger
+        <Menu>
+          <MenuItem href="#second">Sub-menu item</MenuItem>
+        </Menu>
+      </MenuItem>
+    </Menu>,
+  );
+
+  expect(queryByText('Sub-menu item')).not.toBeInTheDocument();
+
+  fireEvent.click(getByText('Sub-menu trigger'));
+
+  expect(queryByText('Sub-menu item')).toBeInTheDocument();
+
+  fireEvent.click(getByText('Sub-menu trigger'));
+
+  expect(queryByText('Sub-menu item')).not.toBeInTheDocument();
+});
+
+test('renders the sub-menu if a sub-menu item is set to active', () => {
+  const { getByText } = render(
+    <Menu>
+      <MenuItem href="#">Top level</MenuItem>
+      <MenuItem>
+        Sub-menu trigger
+        <Menu>
+          <MenuItem href="#second" active={true}>
+            Sub-menu item
+          </MenuItem>
+        </Menu>
+      </MenuItem>
+    </Menu>,
+  );
+
+  expect(getByText('Sub-menu item')).toBeInTheDocument();
+});
+
+test('renders the sub-menu trigger as active if a sub-menu item is set to active', () => {
+  const { getByText } = render(
+    <Menu size="small">
+      <MenuItem href="#">Top level</MenuItem>
+      <MenuItem>
+        Sub-menu trigger
+        <Menu>
+          <MenuItem href="#second" active={true}>
+            Sub-menu item
+          </MenuItem>
+        </Menu>
+      </MenuItem>
+    </Menu>,
+  );
+
+  expect(getByText('Sub-menu trigger').className).toContain('--active');
+});
+
+test('renders a small menu if the small flag is set', () => {
+  const { container } = render(
+    <Menu size="small">
+      <MenuItem href="#">Top level</MenuItem>
+      <MenuItem>
+        Sub-menu trigger
+        <Menu>
+          <MenuItem href="#second">Sub-menu item</MenuItem>
+        </Menu>
+      </MenuItem>
+    </Menu>,
+  );
+
+  expect(container.querySelector('.entur-menu')).toHaveClass(
+    'entur-menu--small',
+  );
+});
+
+test('menu items are disabled if specified', () => {
+  const { getByText } = render(
+    <Menu size="small">
+      <MenuItem href="#" disabled={true}>
+        Disabled item
+      </MenuItem>
+      <MenuItem href="#">Second item</MenuItem>
+    </Menu>,
+  );
+
+  expect(getByText('Disabled item')).toBeDisabled();
 });
