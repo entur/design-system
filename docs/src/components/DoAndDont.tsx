f7fc4fed947fef7da6f574b974211db698fc660f
NicolaiF <nico-fre@hotmail.com>
2019-09-24 10:51:29 +0200

    refactor(DoAndDont): better Api for DoAndDont-component
    affects: @entur/designsystem-docs

--- docs/src/components/DoAndDont.tsx
+++ docs/src/components/DoAndDont.tsx
@@ -1,43 +1,42 @@
 import React from 'react';
 import './DoAndDont.scss';
 
-type DoAndDontProps = {
+function DoAndDont(props: any) {
+  return <div className="do-and-dont-wrapper">{props.children}</div>;
+}
+
+type DoProps = {
   doPicture: string;
-  doList: string[];
-  dontPicture: string;
-  dontList: string[];
+  children: React.ReactNode;
 };
 
-function DoAndDont(props: DoAndDontProps) {
+function Do(props: DoProps) {
   return (
-    <div className="do-and-dont-wrapper">
-      <div className="do-or-dont-container">
-        <div className="do-and-dont-image-wrapper">
-          <img className="do-and-dont-image-container" src={props.doPicture} />
-        </div>
-        <Divider color="green" />
-        <div className="do-header">Do</div>
-        <ul className="do-and-dont-list">
-          {props.doList.map((doElement, index) => (
-            <li key={index}>{doElement}</li>
-          ))}
-        </ul>
+    <div className="do-or-dont-container">
+      <div className="do-and-dont-image-wrapper">
+        <img className="do-and-dont-image-container" src={props.doPicture} />
       </div>
-      <div className="do-or-dont-container">
-        <div className="do-and-dont-image-wrapper">
-          <img
-            className="do-and-dont-image-container"
-            src={props.dontPicture}
-          />
-        </div>
-        <Divider color="red" />
-        <div className="dont-header">Don't</div>
-        <ul className="do-and-dont-list">
-          {props.dontList.map((dontElement, index) => (
-            <li key={index}>{dontElement}</li>
-          ))}
-        </ul>
+      <Divider color="green" />
+      <div className="do-header">Do</div>
+      <ul className="do-and-dont-list">{props.children}</ul>
+    </div>
+  );
+}
+
+type DontProps = {
+  dontPicture: string;
+  children: React.ReactNode;
+};
+
+function Dont(props: DontProps) {
+  return (
+    <div className="do-or-dont-container">
+      <div className="do-and-dont-image-wrapper">
+        <img className="do-and-dont-image-container" src={props.dontPicture} />
       </div>
+      <Divider color="red" />
+      <div className="dont-header">Don't</div>
+      <ul className="do-and-dont-list">{props.children}</ul>
     </div>
   );
 }
@@ -49,5 +48,8 @@ function Divider(props: any) {
     />
   );
 }
+
+DoAndDont.Do = Do;
+DoAndDont.Dont = Dont;
 
 export default DoAndDont;
