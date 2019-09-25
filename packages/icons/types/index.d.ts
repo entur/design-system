6e3cde1a3bca2725d0ab363f5497f22a23179145
Kristofer Selbekk <selbeezy@gmail.com>
2019-09-25 12:53:44 +0200

    chore: remove unused types
    affects: @entur/icons

--- packages/icons/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-declare type IconProps = {
-  'aria-label': string;
-  className?: string;
-};
-declare type ComponentType = React.FC<IconProps>;
-export default ComponentType;
