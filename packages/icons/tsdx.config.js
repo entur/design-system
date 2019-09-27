0dd3991c2a4fdbe3ceefd49405ef2d958f946559
Kristofer Selbekk <selbeezy@gmail.com>
2019-09-27 16:17:09 +0200

    refactor(icons): use svgr rollup plugin
    affects: @entur/icons
    
    This commit makes the build go several times as fast, by skipping writing to disk

--- packages/icons/tsdx.config.js
+++ packages/icons/tsdx.config.js
@@ -1,1 +1,29 @@
-module.exports = require('../../tsdx.config');
+const svgr = require('@svgr/rollup').default;
+const ts = require('rollup-plugin-typescript2');
+
+const svgrConfig = {
+  icon: true,
+  replaceAttrValues: {
+    '#181E53': 'currentColor',
+    '#181C56': 'currentColor',
+  },
+  svgProps: {
+    className: 'entur-icon',
+  },
+};
+
+module.exports = {
+  rollup(config) {
+    config.plugins = [...config.plugins, svgr(svgrConfig)];
+    config.plugins = config.plugins.map(plugin => {
+      if (plugin && plugin.name === 'rpt2') {
+        return ts({
+          ...plugin.options,
+          objectHashIgnoreUnknownHack: true,
+        });
+      }
+      return plugin;
+    });
+    return config;
+  },
+};
