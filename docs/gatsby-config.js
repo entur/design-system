22ddaae7bbadc209b20a3df9d8d4c2ca9c3eb985
Kristofer Selbekk <selbeezy@gmail.com>
2019-10-02 13:27:27 +0200

    chore: remove eslint noise
    affects: @entur/designsystem-docs

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,11 +1,8 @@
 module.exports = {
   plugins: [
     'gatsby-theme-docz',
-    `gatsby-transformer-sharp`,
+    'gatsby-transformer-sharp',
     'gatsby-plugin-sass',
     'gatsby-plugin-typescript',
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 };
