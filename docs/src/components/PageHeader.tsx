394721862f307913a7e553ee9266ad335bb98d7d
NicolaiF <nico-fre@hotmail.com>
2019-10-14 16:39:35 +0200

    feat(docs): added install and import code examples for components
    affects: @entur/designsystem-docs

--- docs/src/components/PageHeader.tsx
+++ docs/src/components/PageHeader.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { useCurrentDoc } from 'docz';
-import { Heading1, Label, LeadParagraph } from '@entur/typography';
+import {
+  Heading1,
+  Label,
+  LeadParagraph,
+  PreformattedText,
+} from '@entur/typography';
+import { useSettings } from './SettingsContext';
 
 type Props = {
   title?: string;
@@ -9,13 +15,28 @@ type Props = {
 
 const PageHeader: React.FC<Props> = ({ title, children, category }) => {
   const currentDoc = useCurrentDoc();
+  const enturPackage: string = currentDoc.enturPackage;
+  const exported: string = currentDoc.exported;
   const categoryToShow = category || currentDoc.parent;
   const titleToShow = title || currentDoc.name;
+  const { packageManager } = useSettings();
+  const installText =
+    packageManager === 'yarn'
+      ? `yarn add @entur/${enturPackage}`
+      : `npm install @entur/${enturPackage}`;
   return (
     <header>
       {categoryToShow && <Label as="div">{categoryToShow.toUpperCase()}</Label>}
       <Heading1 style={{ marginTop: '0.3em' }}>{titleToShow}</Heading1>
       {children && <LeadParagraph>{children}</LeadParagraph>}
+      {(enturPackage || exported) && (
+        <PreformattedText>
+          {enturPackage && installText}
+          <br />
+          {exported !== undefined &&
+            `import \{ ${exported} \} from @entur/${enturPackage}`}
+        </PreformattedText>
+      )}
     </header>
   );
 };
