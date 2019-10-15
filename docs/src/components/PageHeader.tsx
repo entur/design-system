078e3abc7df39864b4c23f792dbad07e5ef50b7b
NicolaiF <nico-fre@hotmail.com>
2019-10-15 12:45:48 +0200

    refactor(docs): npmPackage refactor, and removal of exports from package
    affects: @entur/designsystem-docs

--- docs/src/components/PageHeader.tsx
+++ docs/src/components/PageHeader.tsx
@@ -15,27 +15,21 @@ type Props = {
 
 const PageHeader: React.FC<Props> = ({ title, children, category }) => {
   const currentDoc = useCurrentDoc();
-  const enturPackage: string = currentDoc.enturPackage;
-  const exported: string = currentDoc.exported;
+  const npmPackage: string = currentDoc.npmPackage;
   const categoryToShow = category || currentDoc.parent;
   const titleToShow = title || currentDoc.name;
-  const { packageManager } = useSettings();
+  const { packageManager, userType } = useSettings();
   const installText =
     packageManager === 'yarn'
-      ? `yarn add @entur/${enturPackage}`
-      : `npm install @entur/${enturPackage}`;
+      ? `yarn add @entur/${npmPackage}`
+      : `npm install @entur/${npmPackage}`;
   return (
     <header>
       {categoryToShow && <Label as="div">{categoryToShow.toUpperCase()}</Label>}
       <Heading1 style={{ marginTop: '0.3em' }}>{titleToShow}</Heading1>
       {children && <LeadParagraph>{children}</LeadParagraph>}
-      {(enturPackage || exported) && (
-        <PreformattedText>
-          {enturPackage && installText}
-          <br />
-          {exported !== undefined &&
-            `import \{ ${exported} \} from @entur/${enturPackage}`}
-        </PreformattedText>
+      {npmPackage && userType === 'developer' && (
+        <PreformattedText>{npmPackage && installText}</PreformattedText>
       )}
     </header>
   );
