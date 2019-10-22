1c871feee1cbff914618818179338e037a657e21
NicolaiF <nico-fre@hotmail.com>
2019-10-22 17:14:05 +0200

    docs: click to copy install text
    affects: @entur/designsystem-docs

--- docs/src/components/PageHeader.tsx
+++ docs/src/components/PageHeader.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useCurrentDoc } from 'docz';
+import copy from 'copy-text-to-clipboard';
+import { useToast } from '@entur/alert';
+import { ReportsIcon } from '@entur/icons';
 import {
   Heading1,
   Label,
@@ -7,6 +10,7 @@ import {
   PreformattedText,
 } from '@entur/typography';
 import { useSettings } from './SettingsContext';
+import './PageHeader.scss';
 
 type Props = {
   title?: string;
@@ -14,6 +18,7 @@ type Props = {
 };
 
 const PageHeader: React.FC<Props> = ({ title, children, category }) => {
+  const { addToast } = useToast();
   const currentDoc = useCurrentDoc();
   const npmPackage: string = currentDoc.npmPackage;
   const categoryToShow = category || currentDoc.parent;
@@ -23,13 +28,27 @@ const PageHeader: React.FC<Props> = ({ title, children, category }) => {
     packageManager === 'yarn'
       ? `yarn add @entur/${npmPackage}`
       : `npm install @entur/${npmPackage}`;
+
+  const handleInstallClick = () => {
+    copy(installText);
+    addToast({
+      title: `Innstalleringstekst kopiert!`,
+      content: 'Du finner den i utklippstavla',
+    });
+  };
   return (
     <header>
       {categoryToShow && <Label as="div">{categoryToShow.toUpperCase()}</Label>}
       <Heading1 style={{ marginTop: '0.3em' }}>{titleToShow}</Heading1>
       {children && <LeadParagraph>{children}</LeadParagraph>}
       {npmPackage && userType === 'developer' && (
-        <PreformattedText>{npmPackage && installText}</PreformattedText>
+        <PreformattedText
+          onClick={() => handleInstallClick()}
+          className="page-header__install-text"
+        >
+          {npmPackage && installText}
+          <ReportsIcon inline />
+        </PreformattedText>
       )}
     </header>
   );
