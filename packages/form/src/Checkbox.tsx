ef3738a5e25fdfe81631e87d3406a62f16487eb6
NicolaiF <nico-fre@hotmail.com>
2019-10-25 10:31:02 +0200

    feat: adding checkbox form component
    affects: @entur/form

---
+++ packages/form/src/Checkbox.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { CheckIcon } from '@entur/icons';
+import cx from 'classnames';
+import './Checkbox.scss';
+
+type CheckboxProps = {
+  /** Klasse som sendes til komponenten. Bruk denne om du vil endre style */
+  className?: string;
+  /** Settes til 'fluid' for flytende textarea */
+  width?: 'fluid';
+
+  /** For å deaktivere inputfeltet */
+  disabled?: boolean;
+  [key: string]: any;
+};
+
+export const Checkbox: React.RefForwardingComponent<
+  HTMLInputElement,
+  CheckboxProps
+> = React.forwardRef(
+  (
+    { className, width, disabled = false, ...rest },
+    ref: React.Ref<HTMLInputElement>,
+  ) => {
+    return (
+      <label className={cx('entur-form-component--checkbox')}>
+        <input type="checkbox" ref={ref} {...rest} />
+        <span className="entur-form-component--checkbox__icon">
+          <CheckIcon />
+        </span>
+      </label>
+    );
+  },
+);
