cfdaa176d066b611f305bcb1aedc71d02d07d0c4
NicolaiF <nico-fre@hotmail.com>
2019-10-28 17:02:13 +0100

    refactor(Checkbox): refactoring based on code review
    affects: @entur/form
    
    Also small styling fixes

--- packages/form/src/Checkbox.tsx
+++ packages/form/src/Checkbox.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { CheckIcon } from '@entur/icons';
+import { Label } from '@entur/typography';
 import cx from 'classnames';
 import './Checkbox.scss';
 
 type CheckboxProps = {
   /** Klasse som sendes til komponenten. Bruk denne om du vil endre style */
   className?: string;
-  /** Settes til 'fluid' for flytende textarea */
-  width?: 'fluid';
-
-  /** For å deaktivere inputfeltet */
-  disabled?: boolean;
+  /** Label for checkboxen, som vises ved høyre side. */
+  label?: string;
   [key: string]: any;
 };
 
@@ -18,16 +16,16 @@ export const Checkbox: React.RefForwardingComponent<
   HTMLInputElement,
   CheckboxProps
 > = React.forwardRef(
-  (
-    { className, width, disabled = false, ...rest },
-    ref: React.Ref<HTMLInputElement>,
-  ) => {
+  ({ className, width, label, ...rest }, ref: React.Ref<HTMLInputElement>) => {
     return (
-      <label className={cx('entur-form-component--checkbox__container')}>
+      <label
+        className={cx('entur-form-component--checkbox__container', className)}
+      >
         <input type="checkbox" ref={ref} {...rest} />
         <span className="entur-form-component--checkbox__icon">
           <CheckIcon />
         </span>
+        {label && <Label as="span">{label}</Label>}
       </label>
     );
   },
