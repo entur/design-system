7133c2eb7f04e14b0d012481d7d4f334a57a42e7
NicolaiF <nico-fre@hotmail.com>
2019-10-28 16:59:27 +0100

    feat(RadioGroup): adding RadioGroup for use with radio-buttons
    affects: @entur/form

--- packages/form/src/Radio.tsx
+++ packages/form/src/Radio.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import cx from 'classnames';
-import { Label } from '@entur/typography';
+import { Label, Heading4 } from '@entur/typography';
 import './Radio.scss';
 type RadioProps = {
-  /** Klasse som sendes til komponenten. Bruk denne om du vil endre style */
+  /** Klasse som sendes til komponenten. Bruk denne om du vil endre styling */
   className?: string;
-
-  /** For å deaktivere inputfeltet */
-  disabled?: boolean;
+  /** Label til radio-button. Vises ved høyre side. */
+  label?: string;
+  /** Verdien til radioknappen */
+  value: string;
   [key: string]: any;
 };
 
@@ -15,37 +16,79 @@ export const Radio: React.RefForwardingComponent<
   HTMLInputElement,
   RadioProps
 > = React.forwardRef(
-  (
-    { className, disabled = false, ...rest },
-    ref: React.Ref<HTMLInputElement>,
-  ) => {
-    const classList = cx(className, 'entur-form-component--radio__radio', {
-      [`entur-form-component--disabled`]: disabled,
-    });
+  ({ className, label, value, ...rest }, ref: React.Ref<HTMLInputElement>) => {
+    const classList = cx(className, 'entur-form-component--radio__radio');
+    const { name, value: selectedValue } = useRadioGroupContext();
     return (
       <label className="entur-form-component--radio__container">
-        <input type="radio" disabled={disabled} ref={ref} {...rest} />
+        <input
+          type="radio"
+          name={name}
+          ref={ref}
+          value={value}
+          checked={selectedValue === value}
+          {...rest}
+        />
         <span className={classList}>
           <span className="entur-form-component--radio__circle"></span>
         </span>
+        {label && <Label as="span">{label}</Label>}
       </label>
     );
   },
 );
 
-type RadioLabelProps = {
-  /** Children blir labelteksten */
-  children: React.ReactNode;
+type RadioGroupContextProps = {
+  name: string;
+  value: string;
   [key: string]: any;
 };
 
-export const RadioLabel: React.FC<RadioLabelProps> = ({
+const RadioGroupContext = React.createContext<RadioGroupContextProps | null>(
+  null,
+);
+
+function useRadioGroupContext() {
+  const context = React.useContext(RadioGroupContext);
+  if (!context) {
+    throw new Error(
+      'You need to wrap your RadioButtons in a RadioGroup component',
+    );
+  }
+  return context;
+}
+
+type RadioGroupProps = {
+  /**  */
+  name: string;
+  /** Overskrift over radiogruppen */
+  label?: string;
+  /** Verdien til den valgte radioknappen */
+  value: string;
+  /** Innholdet av Radiogruppen (Radioknapper) */
+  children: React.ReactNode;
+  /** En callback som blir kalles hver gang en radioknapp klikkes på  */
+  onChange: (e: React.ChangeEvent<any>) => void;
+  [key: string]: any;
+};
+
+export const RadioGroup: React.FC<RadioGroupProps> = ({
+  name,
+  value,
+  label,
   children,
   ...rest
 }) => {
   return (
-    <Label {...rest} className="entur-form-component--radio--label">
-      {children}
-    </Label>
+    <RadioGroupContext.Provider value={{ name, value }}>
+      <fieldset className="entur-radio-group" {...rest}>
+        {label && (
+          <Heading4 as="legend" className="entur-radio-group__label">
+            {label}
+          </Heading4>
+        )}
+        {children}
+      </fieldset>
+    </RadioGroupContext.Provider>
   );
 };
