d0708469b26de68237a0d6a139cd77ad86c60959
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2019-11-08 13:47:46 +0100

    feat(ExpandableGroup): add new component ExpandableGroup
    affects: @entur/designsystem-docs, @entur/expand

---
+++ packages/expand/src/ExpandableGroup.test.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { ExpandableGroup, ExpandablePanel } from '.';
+
+test('renders a group of expandable panels that can be opened and closed', () => {
+  const { getByText, queryByTestId } = render(
+    <ExpandableGroup>
+      <ExpandablePanel title="Trains">
+        <div data-testid="expanded-trains">Trains go "choo choo"</div>
+      </ExpandablePanel>
+      <ExpandablePanel title="Boats">
+        <div data-testid="expanded-boats">Boats float</div>
+      </ExpandablePanel>
+      <ExpandablePanel title="Buses">
+        <div data-testid="expanded-buses">Buses go "vroom vroom"</div>
+      </ExpandablePanel>
+    </ExpandableGroup>,
+  );
+
+  expect(queryByTestId('expanded-trains')).not.toBeInTheDocument();
+  expect(queryByTestId('expanded-boats')).not.toBeInTheDocument();
+  expect(queryByTestId('expanded-buses')).not.toBeInTheDocument();
+
+  fireEvent.click(getByText('Boats'));
+
+  expect(queryByTestId('expanded-trains')).not.toBeInTheDocument();
+  expect(queryByTestId('expanded-boats')).toBeInTheDocument();
+  expect(queryByTestId('expanded-buses')).not.toBeInTheDocument();
+
+  fireEvent.click(getByText('Trains'));
+
+  expect(queryByTestId('expanded-trains')).toBeInTheDocument();
+  expect(queryByTestId('expanded-boats')).not.toBeInTheDocument();
+  expect(queryByTestId('expanded-buses')).not.toBeInTheDocument();
+
+  fireEvent.click(getByText('Trains'));
+
+  expect(queryByTestId('expanded-trains')).not.toBeInTheDocument();
+  expect(queryByTestId('expanded-boats')).not.toBeInTheDocument();
+  expect(queryByTestId('expanded-buses')).not.toBeInTheDocument();
+});
+
+test('works with the defaultOpen option', () => {
+  const { getByText, queryByTestId } = render(
+    <ExpandableGroup>
+      <ExpandablePanel title="Trains" defaultOpen>
+        <div data-testid="expanded-trains">Trains go "choo choo"</div>
+      </ExpandablePanel>
+      <ExpandablePanel title="Boats">
+        <div data-testid="expanded-boats">Boats float</div>
+      </ExpandablePanel>
+      <ExpandablePanel title="Buses">
+        <div data-testid="expanded-buses">Buses go "vroom vroom"</div>
+      </ExpandablePanel>
+    </ExpandableGroup>,
+  );
+
+  expect(queryByTestId('expanded-trains')).toBeInTheDocument();
+  expect(queryByTestId('expanded-boats')).not.toBeInTheDocument();
+  expect(queryByTestId('expanded-buses')).not.toBeInTheDocument();
+
+  fireEvent.click(getByText('Boats'));
+
+  expect(queryByTestId('expanded-trains')).not.toBeInTheDocument();
+  expect(queryByTestId('expanded-boats')).toBeInTheDocument();
+  expect(queryByTestId('expanded-buses')).not.toBeInTheDocument();
+});
---
+++ packages/expand/src/ExpandableGroup.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+type Id = string | null;
+type ExpandableGroupContextType = [
+  Id,
+  React.Dispatch<React.SetStateAction<Id>>,
+];
+const ExpandableGroupContext = React.createContext<ExpandableGroupContextType | null>(
+  null,
+);
+
+type Props = {
+  /** To eller flere ExpandablePanel-komponenter */
+  children: React.ReactNode;
+  [key: string]: any;
+};
+
+export const ExpandableGroup: React.FC<Props> = ({ ...rest }) => {
+  const currentlyOpenState = React.useState<Id>(null);
+  return (
+    <ExpandableGroupContext.Provider value={currentlyOpenState} {...rest} />
+  );
+};
+
+type UseExpandableGroupArgs = {
+  id: Id;
+  defaultOpen?: boolean;
+};
+
+export const useExpandableGroup = ({
+  id,
+  defaultOpen,
+}: UseExpandableGroupArgs) => {
+  const context = React.useContext(ExpandableGroupContext);
+  if (!context) {
+    return null;
+  }
+  const [openId, setOpenId] = context;
+  const isOpen = openId === id;
+  React.useEffect(() => {
+    if (defaultOpen) {
+      setOpenId(id);
+    }
+  }, [defaultOpen]);
+  return {
+    isOpen,
+    toggle: () => setOpenId(isOpen ? null : id),
+  };
+};
