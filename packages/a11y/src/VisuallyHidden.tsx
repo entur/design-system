fb6ef995457a3921156114569ed4cac545566d58
NicolaiF <nico-fre@hotmail.com>
2019-12-03 09:22:04 +0100

    fix: improving visuallyhidden by spreading props
    affects: @entur/a11y

--- packages/a11y/src/VisuallyHidden.tsx
+++ packages/a11y/src/VisuallyHidden.tsx
@@ -6,9 +6,15 @@ export type VisuallyHiddenProps = {
   as: string | React.ElementType;
   /** Innhold for skjermlesere */
   children: React.ReactNode;
+  [key: string]: any;
 };
 
 export const VisuallyHidden: React.FC<VisuallyHiddenProps> = ({
   as: Element = 'span',
   children,
-}) => <Element className="eds-visually-hidden">{children}</Element>;
+  ...rest
+}) => (
+  <Element className="eds-visually-hidden" {...rest}>
+    {children}
+  </Element>
+);
