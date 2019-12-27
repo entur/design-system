a98fb429d3b917fc4804a5ceb7a0052ea84052ca
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2019-12-28 00:03:40 +0100

    feat(Checkbox): add support for indeterminate checkboxes
    affects: @entur/form
    
    You can now provide the prop checked="indeterminate" to get an indetermiate checkbox
    
    Closes DS-182

--- packages/form/src/Checkbox.test.tsx
+++ packages/form/src/Checkbox.test.tsx
@@ -10,10 +10,30 @@ test('Checkbox renders with appropriate label and is clickable, and state is set
   );
 
   const checkbox = getByLabelText('Accept terms');
+  expect(checkbox).toHaveProperty('checked', false);
   fireEvent.click(checkbox);
   expect(spy).toHaveBeenCalled();
   expect(checkbox).toHaveProperty('checked', true);
   fireEvent.click(checkbox);
   expect(spy).toHaveBeenCalledTimes(2);
   expect(checkbox).toHaveProperty('checked', false);
+});
+
+test('checkboxes can be indeterminate', () => {
+  const { getByLabelText, rerender } = render(
+    <Checkbox checked="indeterminate" onChange={() => {}}>
+      All
+    </Checkbox>,
+  );
+  expect(getByLabelText('All')).toHaveProperty('indeterminate', true);
+  expect(getByLabelText('All')).toHaveProperty('checked', false);
+
+  rerender(
+    <Checkbox checked={true} onChange={() => {}}>
+      All
+    </Checkbox>,
+  );
+
+  expect(getByLabelText('All')).toHaveProperty('indeterminate', false);
+  expect(getByLabelText('All')).toHaveProperty('checked', true);
 });
