d3f50460a8fa78bbc95b3274bb5678a1a51b9f73
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2020-01-03 11:46:46 +0100

    fix(Dropdown): add a max-height to DropdownList, and improve a11y
    affects: @entur/dropdown
    
    This commit adds a max height of 30 % of the viewport height to the dropdown result list (50 % of
    the viewport height on small devices).
    
    In addition, this commit adds some missing accessibility
    stuff that was missed in previous implementations.
    
    CLOSES DS-183

--- packages/dropdown/src/DropdownList.tsx
+++ packages/dropdown/src/DropdownList.tsx
@@ -18,35 +18,43 @@ export const DropdownList: React.FC<DropdownListProps> = ({
     isOpen,
     selectedItem,
     getItemProps,
+    getMenuProps,
   } = useDownshift();
 
-  if (!isOpen || !items.length) {
-    return null;
-  }
   return (
-    <ul className="eds-dropdown-list" {...rest}>
-      {items.map((item, index) => (
-        <li
-          className={classNames('eds-dropdown-list__item', {
-            'eds-dropdown-list__item--highlighted': highlightedIndex === index,
-            'eds-dropdown-list__item--selected': selectedItem === item,
-          })}
-          {...getItemProps({ key: item.value, item, index })}
-        >
-          <span>{item.label}</span>
-          {item.icons && (
-            <span>
-              {item.icons.map((Icon, index) => (
-                <Icon
-                  key={index}
-                  inline
-                  className="eds-dropdown-list__item-icon"
-                />
-              ))}
-            </span>
-          )}
-        </li>
-      ))}
+    <ul
+      className={classNames('eds-dropdown-list', {
+        'eds-dropdown-list--open': isOpen,
+      })}
+      {...getMenuProps({
+        ...rest,
+      })}
+    >
+      {isOpen
+        ? items.map((item, index) => (
+            <li
+              className={classNames('eds-dropdown-list__item', {
+                'eds-dropdown-list__item--highlighted':
+                  highlightedIndex === index,
+                'eds-dropdown-list__item--selected': selectedItem === item,
+              })}
+              {...getItemProps({ key: item.value, item, index })}
+            >
+              <span>{item.label}</span>
+              {item.icons && (
+                <span>
+                  {item.icons.map((Icon, index) => (
+                    <Icon
+                      key={index}
+                      inline
+                      className="eds-dropdown-list__item-icon"
+                    />
+                  ))}
+                </span>
+              )}
+            </li>
+          ))
+        : null}
     </ul>
   );
 };
