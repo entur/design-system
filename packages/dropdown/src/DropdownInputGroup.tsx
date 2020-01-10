60bdd487a1cd04d0e55363698b095041e4953216
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-01-10 11:41:42 +0100

    feat: add classname prop for dropdown component
    affects: @entur/dropdown

--- packages/dropdown/src/DropdownInputGroup.tsx
+++ packages/dropdown/src/DropdownInputGroup.tsx
@@ -2,30 +2,35 @@ import React from 'react';
 import { Label } from '@entur/typography';
 import { FeedbackText, VariantType, VariantProvider } from '@entur/form';
 import { useDownshift } from './DownshiftProvider';
+import classNames from 'classnames';
 
 export type DropdownInputGroupProps = {
   label?: string;
   feedback?: string;
   variant?: VariantType;
+  className?: string;
 };
 export const DropdownInputGroup: React.FC<DropdownInputGroupProps> = ({
   children,
   label,
   feedback,
   variant,
+  className,
 }) => {
   const { getLabelProps } = useDownshift();
   return (
     <VariantProvider variant={variant}>
-      {label && (
-        <Label {...getLabelProps()} style={{ display: 'block' }}>
-          {label}
-        </Label>
-      )}
-      {children}
-      {feedback && variant && (
-        <FeedbackText variant={variant}>{feedback}</FeedbackText>
-      )}
+      <div className={classNames(className)}>
+        {label && (
+          <Label {...getLabelProps()} style={{ display: 'block' }}>
+            {label}
+          </Label>
+        )}
+        {children}
+        {feedback && variant && (
+          <FeedbackText variant={variant}>{feedback}</FeedbackText>
+        )}
+      </div>
     </VariantProvider>
   );
 };
