0dedd3aeb6780c06cd84a282305c94015e945254
NicolaiF <nico-fre@hotmail.com>
2020-01-15 15:31:24 +0100

    chore: fix config and dependencies for travel components
    affects: @entur/travel

--- packages/travel/tsdx.config.js
+++ packages/travel/tsdx.config.js
@@ -1,1 +1,11 @@
-module.exports = require('../../tsdx.config');
+const sass = require('rollup-plugin-sass');
+const copy = require('rollup-plugin-copy-assets');
+module.exports = {
+  rollup(config) {
+    // TODO for code review: En måte å extende tsdx.confic.js fra rotnivå
+    // For så å legge til copy her
+    config.plugins.unshift(sass({ output: 'dist/styles.css' }));
+    config.plugins.unshift(copy({ assets: ['src/pattern'] }));
+    return config;
+  },
+};
