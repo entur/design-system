7c9f08fddab51a287ed08382b94bc376b833d957
NicolaiF <nico-fre@hotmail.com>
2020-01-16 09:18:49 +0100

    WIP: issues with building travel after merging with master
    affects: @entur/travel

--- packages/travel/tsdx.config.js
+++ packages/travel/tsdx.config.js
@@ -1,11 +1,19 @@
 const sass = require('rollup-plugin-sass');
 const copy = require('rollup-plugin-copy-assets');
+const typescript = require('rollup-plugin-typescript2');
+
 module.exports = {
   rollup(config) {
     // TODO for code review: En måte å extende tsdx.confic.js fra rotnivå
     // For så å legge til copy her
-    config.plugins.unshift(sass({ output: 'dist/styles.css' }));
-    config.plugins.unshift(copy({ assets: ['src/pattern'] }));
+    config.plugins.push(
+      typescript({
+        typescript: require('typescript'),
+        objectHashIgnoreUnknownHack: true,
+      }),
+    );
+    config.plugins.push(copy({ assets: ['src/pattern'] }));
+    config.plugins.push(sass({ output: 'dist/styles.css' }));
     return config;
   },
 };
