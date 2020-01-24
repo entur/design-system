8b0fc894e1ebe6485533a32c2bef8b716d32c4ce
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-01-24 09:24:32 +0100

    chore: use root tsdx config
    affects: @entur/travel

--- packages/travel/tsdx.config.js
+++ packages/travel/tsdx.config.js
@@ -1,8 +1,1 @@
-const sass = require('rollup-plugin-sass');
-
-module.exports = {
-  rollup(config) {
-    config.plugins.push(sass({ output: 'dist/styles.css' }));
-    return config;
-  },
-};
+module.exports = require('../../tsdx.config');
