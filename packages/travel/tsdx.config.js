a35c95fb866c15c5e2d49e9a8fe3e52beb197f80
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-01-24 09:23:16 +0100

    chore: removed unused dependency
    affects: @entur/travel

--- packages/travel/tsdx.config.js
+++ packages/travel/tsdx.config.js
@@ -1,23 +1,7 @@
 const sass = require('rollup-plugin-sass');
-const copy = require('rollup-plugin-copy-assets');
-const rpts2 = require('rollup-plugin-typescript2');
 
 module.exports = {
   rollup(config) {
-    // TODO for code review: En måte å extende tsdx.confic.js fra rotnivå
-    // For så å legge til copy her
-    config.plugins = config.plugins.map(plugin => {
-      if (plugin && plugin.name === 'rpt2') {
-        return rpts2({
-          // properties that I added for demonstration purposes
-          clean: true,
-          objectHashIgnoreUnknownHack: true,
-        });
-      }
-
-      return plugin;
-    });
-    config.plugins.push(copy({ assets: ['src/pattern'] }));
     config.plugins.push(sass({ output: 'dist/styles.css' }));
     return config;
   },
