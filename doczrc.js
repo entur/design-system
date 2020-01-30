e00af12cb27caac5befb1c25c619a3576e9a3478
NicolaiF <nico-fre@hotmail.com>
2020-01-30 10:46:38 +0100

    fix: add content to docz src to (potentially) increase build time

--- doczrc.js
+++ doczrc.js
@@ -2,6 +2,7 @@ export default {
   typescript: true,
   title: 'Entur Designsystem',
   ignore: ['README.md', 'CHANGELOG.md'],
+  src: 'content/',
   docgenConfig: {
     searchPath: 'packages',
   },
