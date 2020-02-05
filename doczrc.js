1c877ba373b1531a1ab1f36594ab6a0de6f77891
NicolaiF <nico-fre@hotmail.com>
2020-02-05 13:34:29 +0100

    fix: fixing broken props on docz build
    and  adding small dependency

--- doczrc.js
+++ doczrc.js
@@ -4,6 +4,6 @@ export default {
   ignore: ['README.md', 'CHANGELOG.md', 'packages/*/node_modules'],
   src: 'content',
   docgenConfig: {
-    searchPath: 'packages/*/dist',
+    searchPath: 'packages',
   },
 };
