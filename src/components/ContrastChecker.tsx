ffb71da7a722bea2547828a5c10c57fbad1a77c5
NicolaiF <nico-fre@hotmail.com>
2020-02-07 13:46:44 +0100

    feat: contrast checker component for documentation

---
+++ src/components/ContrastChecker.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { hex } from 'wcag-contrast';
+import { colors } from '@entur/tokens';
+import { Dropdown } from '@entur/dropdown';
+import { flatten } from '~/utils/flatten';
+
+function ContrastChecker() {
+  const flatColors = flatten(colors);
+  const lis = [];
+  for (let [key, value] of Object.entries(flatColors)) {
+    lis.push({ label: key, value: value });
+  }
+  const [text, setText] = React.useState(lis[0]);
+  const [background, setBackground] = React.useState(lis[2]);
+  const score = hex(text.value, background.value);
+
+  return (
+    <div>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr',
+          gridGap: '1rem',
+          marginBottom: '1rem',
+        }}
+      >
+        <div>
+          <Dropdown
+            label="Tekst"
+            items={lis}
+            onChange={item =>
+              setText({ value: item!.value, label: item!.label })
+            }
+            placeholder={text.label}
+          />
+          <Dropdown
+            items={lis}
+            label="Bakgrunn"
+            onChange={item =>
+              setBackground({ value: item!.value, label: item!.label })
+            }
+            placeholder={background.label}
+            style={{ marginTop: '0' }}
+          />
+        </div>
+        <div
+          style={{
+            color: text.value,
+            background: background.value,
+            border: `4px solid ${colors.blues.blue20}`,
+            padding: '0.5rem',
+          }}
+        >
+          Tekst på bakgrunn
+        </div>
+      </div>
+      <div>
+        Kontrast:{score}
+        <div>{score > 4.5 ? 'WCAG AA Godkjent' : 'WCAG AA Ikke godkjent'}</div>
+        <div>{score > 7 ? 'WCAG AAA Godkjent' : 'WCAG AAA Ikke godkjent'}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ContrastChecker;
