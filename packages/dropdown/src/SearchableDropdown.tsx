5c4758ef37eeabf86ae6f79a3c5ff53a46003d0a
Eirik Vageskar <eirikvageskar@gmail.com>
2020-02-13 10:03:35 +0100

    fix: Remove prop which is not used
    affects: @entur/dropdown

--- packages/dropdown/src/SearchableDropdown.tsx
+++ packages/dropdown/src/SearchableDropdown.tsx
@@ -26,7 +26,6 @@ export const SearchableDropdown: React.FC<SearchableDropdownProps> = ({
   prepend,
   selectOnTab = false,
   openOnFocus = false,
-  autoHighlightFirstItem = false,
   ...rest
 }) => {
   const {
