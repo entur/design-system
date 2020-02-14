57234bb8a7f1706069aa41ddd6da954df9347529
NicolaiF <nico-fre@hotmail.com>
2020-02-14 13:53:35 +0100

    chore: config changes to try fix failing build
    affects: @entur/typography

--- doczrc.js
+++ doczrc.js
@@ -1,11 +1,9 @@
-const searchPath =
-  process.env.NODE_ENV === 'production' ? 'packages' : 'packages/*/src';
 export default {
   typescript: true,
   title: 'Entur Designsystem',
   ignore: ['README.md', 'CHANGELOG.md', 'packages/*/node_modules'],
   src: 'content',
   docgenConfig: {
-    searchPath: searchPath,
+    searchPath: 'packages',
   },
 };
