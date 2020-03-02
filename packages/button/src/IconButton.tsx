81e966f3be5cac8605a26abe7f558aeb3682aa1c
NicolaiF <nico-fre@hotmail.com>
2020-03-02 19:04:47 +0100

    feat(iconbutton): add iconbutton component
    affects: @entur/button
    
    for use in tables etc

---
+++ packages/button/src/IconButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import classNames from 'classnames';
+import './IconButton.scss';
+
+export type IconButtonProps = {
+  /** Ikonet som du vil ha inne i knappen */
+  children: React.ReactNode;
+  /** Ekstra klassenavn */
+  className?: string;
+  /** Deaktivering av knappen
+   * @default false
+   */
+  disabled?: boolean;
+  /** HTML-elementet eller React-komponenten som lager knappen
+   * @default 'button'
+   */
+  as?: 'a' | 'button' | React.ElementType;
+  [key: string]: any;
+};
+
+export const IconButton: React.FC<IconButtonProps> = ({
+  children,
+  className,
+  variant,
+  disabled = false,
+  as = 'button',
+  ...rest
+}) => {
+  const Element = disabled ? 'button' : as;
+
+  return (
+    <Element className={classNames('eds-icon-button', className)} {...rest}>
+      {children}
+    </Element>
+  );
+};
