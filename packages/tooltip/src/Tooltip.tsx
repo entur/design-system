6b27444fbc18b975b721ee8a17936aa846b61baf
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-03-16 16:48:46 +0100

    fix: rework tooltip component to increase usability
    May be breaking if previous use included useRef/ref
    
    affects: @entur/tooltip

--- packages/tooltip/src/Tooltip.tsx
+++ packages/tooltip/src/Tooltip.tsx
@@ -1,27 +1,29 @@
-import React, { cloneElement } from 'react';
-import { useTooltip, TooltipPopup } from '@reach/tooltip';
-
+import React, { cloneElement, useState } from 'react';
+import { Manager, Reference, Popper } from 'react-popper';
 import classNames from 'classnames';
+import { useRandomId } from '@entur/utils';
+import { Placement } from 'popper.js';
 
 export type TooltipProps = {
   /** Plassering av tooltip-en */
   placement:
-    | 'top-left'
+    | 'top-left' // top-start
     | 'top'
-    | 'top-right'
+    | 'top-right' // top-end
     | 'left'
     | 'right'
-    | 'bottom-left'
+    | 'bottom-left' // bottom-start
     | 'bottom'
-    | 'bottom-right';
+    | 'bottom-right'; // bottom-end
   /** Innholdet i tooltip-boksen */
-  content: string;
+  content: React.ReactNode;
   /** Elementet som skal ha tooltip-funksjonalitet */
   children: React.ReactElement;
   /** Ekstra klassenavn */
   className?: string;
   [key: string]: any;
 };
+
 export const Tooltip: React.FC<TooltipProps> = ({
   placement,
   content,
@@ -29,33 +31,61 @@ export const Tooltip: React.FC<TooltipProps> = ({
   className,
   ...rest
 }) => {
-  // get the props from useTooltip
-  const [trigger, tooltip] = useTooltip();
-  // destructure off what we need to position the triangle
-  // const { isVisible, triggerRect } = tooltip;
-
-  // const [showTooltip, setShowTooltip] = useState(false);
+  const [showTooltip, setShowTooltip] = useState(false);
+  const tooltipId = useRandomId('eds-tooltip');
+  console.log(showTooltip);
+  if (placement.includes('-')) {
+    if (placement.includes('right')) {
+      placement.replace('right', 'end');
+    }
+    if (placement.includes('left')) {
+      placement.replace('left', 'start');
+    }
+  }
+  const popperPlacement = placement as Placement;
   return (
-    <>
-      {cloneElement(children, trigger)}
-      <div
-        className="eds-tooltip-wrapper"
-        // onMouseLeave={() => setShowTooltip(false)}
-      >
-        <TooltipPopup
-          className={classNames(
-            'eds-tooltip',
-            className,
-            `eds-tooltip--${placement}`,
+    <Manager>
+      <Reference>
+        {({ ref }) =>
+          cloneElement(children, {
+            ref: ref,
+            onMouseLeave: () => setShowTooltip(false),
+            onMouseEnter: () => setShowTooltip(true),
+            'aria-describedby': tooltipId,
+            onFocus: setShowTooltip(true),
+            onBlur: setShowTooltip(false),
+          })
+        }
+      </Reference>
+      {true && (
+        <Popper
+          modifiers={{
+            arrow: { enabled: false },
+            offset: { offset: '0, 8' },
+            // computeStyle: { enabled: false },
+            // applyStyle: { enabled: false },
+          }}
+          placement={popperPlacement}
+        >
+          {({ ref, style, placement: popperPlacement }) => (
+            <div
+              className={classNames(
+                'eds-tooltip',
+                className,
+                `eds-tooltip--${popperPlacement}`,
+              )}
+              ref={ref}
+              style={style}
+              role="tooltip"
+              id={tooltipId}
+              data-placement={popperPlacement}
+              {...rest}
+            >
+              {content}
+            </div>
           )}
-          role="tooltip"
-          // aria-hidden={!showTooltip}
-          {...tooltip}
-          label={content}
-          // position={placement}
-          {...rest}
-        ></TooltipPopup>
-      </div>
-    </>
+        </Popper>
+      )}
+    </Manager>
   );
 };
