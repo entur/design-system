cfb67d41eb169f917ced88a35f540120d0d58a78
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-03-18 12:04:52 +0100

    fix: update shadow tokens
    affects: @entur/tokens
    
    and add contrast shadow

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -91,7 +91,10 @@ export const colors = {
 
 export const shadows = {
   focus: `0 0 0.125rem 0.125rem ${colors.misc.focus}`,
-  cardShadow: '0 0.125rem 0.25rem 0.125rem rgba(0, 0, 0, 0.2)',
+  cardShadow: `0 2px 3px 0 ${colors.greys.grey10}`, //B3 er 70% alpha
+  cardShadowHover: `0 2px 16px 0 ${colors.greys.grey10}`,
+  cardShadowContrast: `0 2px 3px 0 ${colors.blues.blue20}`, //B3 er 70% alpha
+  cardShadowHoverContrast: `0 2px 16px 0 ${colors.blues.blue20}`,
 };
 
 // Dimensions
