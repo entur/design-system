0e5e90aedff1e1fd2f927de51133356b41eae58c
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-03-20 09:54:02 +0100

    feat: add hover and active props for tablerow
    affects: @entur/table

--- packages/table/src/TableHead.tsx
+++ packages/table/src/TableHead.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import classNames from 'classnames';
 
 export type TableHeadProps = {
   /** Kolonneoverskrifter */
   children: React.ReactNode;
+  /** Esktra klassenavn */
+  className?: string;
   [key: string]: any;
 };
 
 export const TableHead: React.FC<TableHeadProps> = props => (
-  <thead {...props} />
+  <thead
+    className={classNames('eds-table__head', props.className)}
+    {...props}
+  />
 );
