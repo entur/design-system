ab32ce5aa57f551a92baac39a9b3c2b9cb87f5fe
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-03-25 15:38:21 +0100

    test: add test for fieldset removal if no label
    affects: @entur/form

--- packages/form/src/Radio.test.tsx
+++ packages/form/src/Radio.test.tsx
@@ -37,3 +37,28 @@ test('Radio buttons works nicely', () => {
   expect(firstOption).toHaveProperty('checked', false);
   expect(secondOption).toHaveProperty('checked', true);
 });
+
+test('Removes fieldset if label is not set', () => {
+  const spy = jest.fn();
+
+  const { getByLabelText, container, rerender } = render(
+    <RadioGroup name="city" value="Oslo" onChange={spy}>
+      <Radio value="Oslo">Oslo</Radio>
+      <Radio value="Bergen">Bergen</Radio>
+    </RadioGroup>,
+  );
+  expect(getByLabelText('Oslo')).toHaveProperty('checked', true);
+  expect(container.nodeName).toBe('DIV');
+  expect(container.firstChild!.nodeName).toBe('LABEL');
+  rerender(
+    <RadioGroup name="city" label="Velg by" value="Bergen" onChange={spy}>
+      <Radio label="Oslo" value="Oslo">
+        Oslo
+      </Radio>
+      <Radio label="Bergen" value="Bergen">
+        Bergen
+      </Radio>
+    </RadioGroup>,
+  );
+  expect(container.firstChild!.nodeName).toBe('FIELDSET');
+});
