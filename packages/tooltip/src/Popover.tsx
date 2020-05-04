e9c64dca21ac28ad29dc825de5c79805139d8275
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-05-04 15:30:47 +0200

    chore: popover rewrite, and basic docs
    affects: @entur/tooltip

--- packages/tooltip/src/Popover.tsx
+++ packages/tooltip/src/Popover.tsx
@@ -1,76 +1,143 @@
-import React, { cloneElement } from 'react';
-import { Manager, Reference, Popper } from 'react-popper';
+import React, { cloneElement, createContext, useContext } from 'react';
+import { usePopper } from 'react-popper';
 import classNames from 'classnames';
 import './Popover.scss';
+
+type PopoverContextProps = {
+  showPopover: boolean;
+  referenceElement: React.RefObject<HTMLButtonElement>;
+  popperElement: React.RefObject<HTMLDivElement>;
+  arrowElement: React.RefObject<HTMLDivElement>;
+  styles: { [key: string]: React.CSSProperties };
+  attributes: { [key: string]: { [key: string]: string } };
+  buttonProps: {};
+  contentProps: {};
+};
+
+const PopoverContext = createContext<PopoverContextProps | undefined>(
+  undefined,
+);
+const usePopoverContext = () => {
+  const context = useContext(PopoverContext);
+  if (context == null) {
+    throw Error('usePopoverContext must be used within <Popover/>');
+  }
+  return context;
+};
 
 type PopoverProps = {
   /** Innholdet i Popover'en */
   children: React.ReactNode;
-  /** Komponenten som skal brukes for å åpne Popoveren */
-  button: React.ReactElement;
-  /** Ekstra klassenavn */
-  className?: string;
-  [key: string]: any;
 };
 
-export const Popover: React.FC<PopoverProps> = ({
-  children,
-  className,
-  button,
-  ...rest
-}) => {
-  const [showPopover, setShowPopover] = React.useState(true);
-  const childProps = {
-    onClick: () => setShowPopover(!showPopover),
+export const Popover: React.FC<PopoverProps> = ({ children }) => {
+  const [showPopover, setShowPopover] = React.useState(false);
+
+  const triggerElement = React.useRef(null);
+  const contentElement = React.useRef(null);
+  const arrowElement = React.useRef(null);
+  console.log(contentElement);
+
+  const { styles, attributes } = usePopper(
+    triggerElement.current,
+    contentElement.current,
+    {
+      modifiers: [
+        { name: 'arrow', options: { element: arrowElement.current } },
+        { name: 'placement', options: { placement: 'bottom' } },
+        {
+          name: 'offset',
+          options: {
+            offset: [0, 10],
+          },
+        },
+      ],
+    },
+  );
+
+  const buttonProps = {
+    onClick: () => {
+      setShowPopover(!showPopover);
+      //@ts-ignore
+      contentElement.current.focus();
+    },
+    'aria-haspopup': 'dialog',
+    'aria-expanded': showPopover,
+  };
+
+  const handleBlur = (event: React.FocusEventHandler<HTMLElement>) => {
+    if (
+      showPopover &&
+      contentElement.current &&
+      //@ts-ignore
+      !triggerElement.current.contains(event.relatedTarget) &&
+      //@ts-ignore
+      !contentElement.current.contains(event.relatedTarget)
+    ) {
+      setShowPopover(false);
+    }
+  };
+
+  const contentProps = {
+    role: 'dialog',
+    'aria-modal': 'false',
+    onBlur: handleBlur,
+    ref: contentElement,
+    onKeyDown: (event: React.KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        showPopover && setShowPopover(false);
+      }
+    },
+  };
+  const contextValue = {
+    showPopover,
+    referenceElement: triggerElement,
+    popperElement: contentElement,
+    arrowElement: arrowElement,
+    styles,
+    attributes,
+    buttonProps,
+    contentProps,
   };
   return (
-    <Manager>
-      <Reference>
-        {({ ref }) =>
-          cloneElement(button, {
-            ref: ref,
-            ...childProps,
-          })
-        }
-      </Reference>
-      {showPopover && (
-        <Popper
-          placement="bottom"
-          modifiers={[
-            {
-              name: 'arrow',
-              options: {
-                enabled: true,
-              },
-            },
-            {
-              name: 'offset',
-              options: {
-                offset: [0, 10],
-              },
-            },
-          ]}
-        >
-          {({ style, placement, ref }) => {
-            return (
-              <div
-                className={classNames(
-                  className,
-                  'eds-popover',
-                  `eds-popover--${placement}`,
-                )}
-                ref={ref}
-                style={style}
-                data-placement={placement}
-                onBlur={() => setShowPopover(false)}
-                {...rest}
-              >
-                {children}
-              </div>
-            );
-          }}
-        </Popper>
-      )}
-    </Manager>
+    <PopoverContext.Provider value={contextValue}>
+      {children}
+    </PopoverContext.Provider>
   );
 };
+
+export const PopoverTrigger: React.FC<{ children: React.ReactElement }> = ({
+  children,
+}) => {
+  const { buttonProps, referenceElement } = usePopoverContext();
+  return cloneElement(children, { ref: referenceElement, ...buttonProps });
+};
+
+export const PopoverContent: React.RefForwardingComponent<
+  HTMLDivElement,
+  { children: React.ReactNode }
+> = React.forwardRef(({ children }, ref: React.Ref<HTMLDivElement>) => {
+  const {
+    showPopover,
+    attributes,
+    arrowElement,
+    styles,
+    contentProps,
+  } = usePopoverContext();
+
+  return (
+    <div
+      className={classNames('eds-popover', {
+        'eds-popover--hidden': !showPopover,
+      })}
+      ref={ref}
+      style={styles.popper}
+      aria-hidden={!showPopover}
+      {...attributes.styles}
+      {...contentProps}
+    >
+      {children}
+      <div ref={arrowElement} style={styles.arrow}></div>
+    </div>
+  );
+});
