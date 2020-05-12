45e4e1b4c60e0e94828c0144b9c16473e315eb54
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-05-12 13:02:57 +0200

    fix: prop to remove active line, and min width adjustments for tab
    affects: @entur/tab
    
    min width reduced from 10-8 rem
    removed min width if tablist has width=fluid
    
    ISSUES CLOSED: DS-358

--- packages/tab/src/Tab.tsx
+++ packages/tab/src/Tab.tsx
@@ -9,9 +9,23 @@ type TabProps = {
   disabled?: boolean;
   /** HTML-elementet eller React-komponenten som lager komponenten */
   as?: keyof JSX.IntrinsicElements | any;
+  removeActiveLine?: boolean;
   [key: string]: any;
 };
 
-export const Tab: React.FC<TabProps> = ({ className, ...rest }) => {
-  return <ReachTab className={classNames('eds-tab', className)} {...rest} />;
+export const Tab: React.FC<TabProps> = ({
+  className,
+  removeActiveLine = false,
+  ...rest
+}) => {
+  return (
+    <ReachTab
+      className={classNames(
+        'eds-tab',
+        { 'eds-tab--remove-active-line': removeActiveLine },
+        className,
+      )}
+      {...rest}
+    />
+  );
 };
