33f8f0a5eb2bbc397f1f9c181e015b1ca630c512
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-07-02 14:36:16 +0200

    test: add test for itemfilter
    affects: @entur/dropdown

--- packages/dropdown/src/Dropdown.test.tsx
+++ packages/dropdown/src/Dropdown.test.tsx
@@ -76,6 +76,63 @@ test('renders a searchable dropdown', async () => {
   fireEvent.click(getByText('Bergen'));
   expect(changeSpy).toHaveBeenCalled();
 });
+
+test('renders a searchable dropdown, with no filtering', async () => {
+  const changeSpy = jest.fn();
+  const { queryByText, getByText, getByPlaceholderText } = render(
+    <Dropdown
+      items={testItems}
+      placeholder="Velg noe"
+      onChange={changeSpy}
+      searchable
+      itemFilter={() => true}
+    />,
+  );
+
+  const inputField = getByPlaceholderText('Velg noe');
+
+  act(() => {
+    fireEvent.focus(inputField);
+    fireEvent.change(inputField, { target: { value: 'er' } });
+  });
+
+  await wait(() => getByText('Bergen'));
+
+  expect(getByText('Bergen')).toBeInTheDocument();
+  expect(getByText('Stavanger')).toBeInTheDocument();
+  expect(queryByText('Oslo')).toBeInTheDocument();
+});
+
+test('renders a searchable dropdown, with strict filter', async () => {
+  const changeSpy = jest.fn();
+  const { queryByText, getByText, getByPlaceholderText } = render(
+    <Dropdown
+      items={testItems}
+      placeholder="Velg noe"
+      onChange={changeSpy}
+      searchable
+      itemFilter={item => item.label === 'Bergen'}
+    />,
+  );
+
+  const inputField = getByPlaceholderText('Velg noe');
+  expect(queryByText('Bergen')).not.toBeInTheDocument();
+
+  act(() => {
+    fireEvent.focus(inputField);
+    fireEvent.change(inputField, { target: { value: 'Berg' } });
+  });
+
+  await wait(() => getByText('Bergen'));
+
+  expect(getByText('Bergen')).toBeInTheDocument();
+  expect(queryByText('Stavanger')).not.toBeInTheDocument();
+  expect(queryByText('Oslo')).not.toBeInTheDocument();
+
+  fireEvent.click(getByText('Bergen'));
+  expect(changeSpy).toHaveBeenCalled();
+});
+
 test('handles all sorts of items', () => {
   const spy = jest.fn();
   const MockIcon = () => <svg />;
