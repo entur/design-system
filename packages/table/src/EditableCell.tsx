3f6efeed5c26a9112dbf2f91edc67be72a25c914
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-08-21 10:22:18 +0200

    fix(editable cell): rework editable cell api
    affects: @entur/table
    
    now you must provide the input components directly
    
    BREAKING CHANGE:
    will be breaking for editable cell users | use input components directly (see documentation),
    instead of value prop

--- packages/table/src/EditableCell.tsx
+++ packages/table/src/EditableCell.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { DataCell } from './DataCell';
 import classNames from 'classnames';
+import React from 'react';
+import { DataCell } from './DataCell';
+import { InputGroup } from '@entur/form';
 import './EditableCell.scss';
 
 type EditableCellProps = {
@@ -9,40 +10,38 @@ type EditableCellProps = {
   /** En callback som blir kalles hver gang innholdet endres  */
   onChange?: (e: React.ChangeEvent) => void;
   /** Verdien til cellen */
-  value: string;
+  value?: string;
+  children: React.ReactElement;
   [key: string]: any;
 };
 
 export const EditableCell: React.FC<EditableCellProps> = ({
+  children,
   value,
   onChange,
   className,
   name,
+  feedback,
+  variant,
+
   ...rest
 }) => {
-  const [editing, setEditing] = useState(rest.open ? true : false);
+  // const [editing, setEditing] = useState(rest.open ? true : false);
+  console.log(variant);
 
   return (
     <DataCell className={classNames('eds-editable-cell', className)} {...rest}>
-      {editing ? (
-        <input
-          type="text"
-          className="eds-editable-cell__cell eds-editable-cell__input"
-          value={value}
-          autoFocus
-          onBlur={() => setEditing(false)}
-          onChange={onChange}
-          name={name}
-        />
-      ) : (
-        <button
-          className="eds-editable-cell__cell eds-editable-cell__activate-button"
-          onClick={() => setEditing(true)}
-          onFocus={() => setEditing(true)}
-        >
-          {value}
-        </button>
-      )}
+      <InputGroup feedback={feedback} variant={variant} label="ok">
+        {/* {React.cloneElement(children, {
+          className: 'eds-editable-cell__cell eds-editable-cell__input',
+          // value: { value },
+          // autoFocus: true,
+          // onBlur: () => setEditing(false),
+          // onChange: { onChange },
+          // name: { name },
+        })} */}
+        {children}
+      </InputGroup>
     </DataCell>
   );
 };
