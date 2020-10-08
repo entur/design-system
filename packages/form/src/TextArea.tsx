a7b6ff30d26de1daa8a97bea35fc540641caefff
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-10-08 14:41:06 +0200

    feat(textarea): add inputgroup props to textarea
    affects: @entur/form
    
    label, labeltooltip and feedback added to textarea
    
    BREAKING CHANGE:
    label is now required prop for textarea

--- packages/form/src/TextArea.tsx
+++ packages/form/src/TextArea.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { VariantType } from './VariantProvider';
 import { BaseFormControl } from './BaseFormControl';
 import './TextArea.scss';
+import { isFilled } from './utils';
+import { useInputGroupContext } from './InputGroupContext';
+import { useRandomId } from '@entur/utils';
 
 export type TextAreaProps = {
   /** Ekstra klassenavn */
@@ -12,14 +15,31 @@ export type TextAreaProps = {
   disabled?: boolean;
   /** Setter tekstområdet i read-only modus */
   readOnly?: boolean;
+  /** Label over TextArea */
+  label: string;
+  /** En tooltip som forklarer labelen til inputfeltet */
+  labelTooltip: React.ReactNode;
+  /** Varselmelding, som vil komme under TextArea */
+  feedback?: string;
   [key: string]: any;
 };
 
 export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
   (
-    { variant, disabled = false, readOnly = false, className, style, ...rest },
+    {
+      variant,
+      disabled = false,
+      readOnly = false,
+      className,
+      style,
+      label,
+      feedback,
+      labelTooltip,
+      ...rest
+    },
     ref: React.Ref<HTMLTextAreaElement>,
   ) => {
+    const textAreaId = useRandomId('eds-textarea');
     return (
       <BaseFormControl
         className={className}
@@ -27,15 +47,58 @@ export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
         readOnly={readOnly}
         variant={variant}
         style={style}
+        label={label}
+        labelId={textAreaId}
+        feedback={feedback}
       >
-        <textarea
-          className="eds-form-control eds-textarea"
-          ref={ref}
+        <TextAreaBase
           readOnly={readOnly}
           disabled={disabled}
+          ref={ref}
+          aria-labelledby={textAreaId}
           {...rest}
         />
       </BaseFormControl>
+    );
+  },
+);
+
+type TextAreaBaseProps = {
+  readOnly?: boolean;
+  disabled?: boolean;
+  [key: string]: any;
+};
+
+const TextAreaBase = React.forwardRef<HTMLTextAreaElement, TextAreaBaseProps>(
+  ({ readOnly, disabled, ...rest }, ref) => {
+    const { isFilled: k, setFilled: setFiller } = useInputGroupContext();
+
+    React.useEffect(() => {
+      // Check if filled on first render
+      if (rest.value) {
+        setFiller && !k && setFiller(true);
+      }
+    }, []);
+
+    const handleChange = (event: any) => {
+      if (isFilled(event.target)) {
+        setFiller && !k && setFiller(true);
+      } else {
+        setFiller && k && setFiller(false);
+      }
+      if (rest.onChange) {
+        rest.onChange();
+      }
+    };
+    return (
+      <textarea
+        className="eds-form-control eds-textarea"
+        ref={ref}
+        readOnly={readOnly}
+        disabled={disabled}
+        onChange={handleChange}
+        {...rest}
+      />
     );
   },
 );
