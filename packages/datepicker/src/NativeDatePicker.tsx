6f09ad3fc09d976d0807bf2abc91f0763f02b654
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-10-08 15:36:30 +0200

    feat(nativedatepicker): add input group props to nativedatepicker
    affects: @entur/datepicker
    
    label feedback and variant
    
    BREAKING CHANGE:
    label is required

--- packages/datepicker/src/NativeDatePicker.tsx
+++ packages/datepicker/src/NativeDatePicker.tsx
@@ -1,29 +1,93 @@
 import React from 'react';
-import { BaseFormControl } from '@entur/form';
+import {
+  BaseFormControl,
+  isFilled,
+  useInputGroupContext,
+  useVariant,
+  VariantType,
+} from '@entur/form';
 import { DateIcon } from '@entur/icons';
+import { useRandomId } from '@entur/utils';
 
 export type NativeDatePickerProps = {
   /** Ekstra klassenavn */
   className?: string;
+  /** Label over NativeDatePicker */
+  label: string;
+  /** Varselmelding, som vil komme under NativeDatePicker */
+  feedback?: string;
+  /** Valideringsvariant */
+  variant?: VariantType;
   [key: string]: any;
 };
 
 export const NativeDatePicker = React.forwardRef<
   HTMLInputElement,
   NativeDatePickerProps
->(({ className, style, ...rest }, ref: React.Ref<HTMLInputElement>) => {
+>(
+  (
+    { className, style, label, onChange, feedback, variant, ...rest },
+    ref: React.Ref<HTMLInputElement>,
+  ) => {
+    const nativedatepickerId = useRandomId('eds-nativetimepicker');
+    return (
+      <BaseFormControl
+        style={style}
+        className={className}
+        prepend={<DateIcon inline />}
+        label={label}
+        feedback={feedback}
+        variant={variant}
+        labelId={nativedatepickerId}
+      >
+        <NativeDatePickerBase
+          onChange={onChange}
+          id={nativedatepickerId}
+          ref={ref}
+          {...rest}
+        />
+      </BaseFormControl>
+    );
+  },
+);
+
+type NativeDatePickerBaseProps = {
+  onChange?: any;
+  [key: string]: any;
+};
+
+const NativeDatePickerBase = React.forwardRef<
+  HTMLInputElement,
+  NativeDatePickerBaseProps
+>(({ onChange, id, ...rest }, ref) => {
+  const contextVariant = useVariant();
+  const currentVariant = rest.variant || contextVariant;
+  const { isFilled: k, setFilled: setFiller } = useInputGroupContext();
+
+  React.useEffect(() => {
+    // Check if filled on first render
+    setFiller && !k && setFiller(true);
+  }, []);
+
+  const handleChange = (event: any) => {
+    if (isFilled(event.target)) {
+      setFiller && !k && setFiller(true);
+    } else {
+      setFiller && k && setFiller(false);
+    }
+    if (rest.onChange) {
+      rest.onChange(event);
+    }
+  };
   return (
-    <BaseFormControl
-      style={style}
-      className={className}
-      prepend={<DateIcon inline />}
-    >
-      <input
-        ref={ref}
-        type="date"
-        className="eds-form-control eds-native-date-picker"
-        {...rest}
-      />
-    </BaseFormControl>
+    <input
+      ref={ref}
+      aria-invalid={currentVariant === 'error'}
+      type="date"
+      className="eds-form-control eds-native-date-picker"
+      onChange={handleChange}
+      id={id}
+      {...rest}
+    />
   );
 });
