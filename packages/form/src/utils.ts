f09edfc1f407ea2f3d99293773d6d8bf786d44b2
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-10-08 14:32:42 +0200

    chore(utils): add utils for helper functions
    affects: @entur/form

---
+++ packages/form/src/utils.ts
@@ -0,0 +1,18 @@
+export function hasValue(value: any) {
+  return value != null && !(Array.isArray(value) && value.length === 0);
+}
+
+// Determine if field is empty or filled.
+// Response determines if label is presented above field or as placeholder.
+//
+// @param obj
+// @param SSR
+// @returns {boolean} False when not present or empty string.
+//                    True when any number or string with length.
+export function isFilled(obj: any, SSR = false) {
+  return (
+    obj &&
+    ((hasValue(obj.value) && obj.value !== '') ||
+      (SSR && hasValue(obj.defaultValue) && obj.defaultValue !== ''))
+  );
+}
