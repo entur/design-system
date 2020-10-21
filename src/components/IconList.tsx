cffd6f3bd6553dcab97ebb8bced62f321db5998f
NicolaiF <nico-fre@hotmail.com>
2020-10-21 14:55:15 +0200

    docs: improve filtering on icons site

--- src/components/IconList.tsx
+++ src/components/IconList.tsx
@@ -5,6 +5,7 @@ import { Switch, TextField } from '@entur/form';
 import { DownloadIcon, ReportsIcon, SearchIcon } from '@entur/icons';
 import { fontSizes } from '@entur/tokens';
 import { Tooltip } from '@entur/tooltip';
+import { GridContainer, GridItem } from '@entur/grid';
 import { Heading4, SubLabel } from '@entur/typography';
 import classNames from 'classnames';
 import copy from 'copy-text-to-clipboard';
@@ -18,33 +19,83 @@ type IconListProps = {
     [key: string]: React.ElementType;
   };
 };
+const ICON_SIZES = [
+  { label: 'Small', value: fontSizes.small.toString() },
+  { label: 'Medium', value: fontSizes.medium.toString() },
+  { label: 'Large', value: fontSizes.large.toString() },
+  { label: '2XLarge', value: fontSizes.extraLarge2.toString() },
+  { label: '3XLarge', value: fontSizes.extraLarge3.toString() },
+  { label: '4XLarge', value: fontSizes.extraLarge4.toString() },
+];
+
+function groupBy(list, keyGetter) {
+  const map = new Map();
+  list.forEach(item => {
+    const key = keyGetter(item);
+    const collection = map.get(key);
+    if (!collection) {
+      map.set(key, [item]);
+    } else {
+      collection.push(item);
+    }
+  });
+  return map;
+}
+
+const unique = (value, index, self) => {
+  return self.indexOf(value) === index;
+};
+
 const IconList: React.FC<IconListProps> = props => {
+  const { addToast } = useToast();
+  const iconsQuery = useGetIcons();
   const [isContrast, setContrast] = React.useState(false);
   const [filterString, setFilterString] = React.useState('');
   const [iconSize, setIconSize] = React.useState<{
     label: string;
     value: string;
   } | null>({
-    label: 'Medium',
+    label: '2XLarge',
     value: fontSizes.extraLarge2.toString(),
   });
+  const [category, setCategory] = React.useState<{
+    value: string;
+    label: string;
+  } | null>({ value: '', label: '' });
 
-  const [category, setCategory] = React.useState<string | null>(null);
+  const categoriesMap = iconsQuery.map(icon => {
+    return {
+      category: icon.node.absolutePath.split('/').splice(-2, 1).toString(),
+      name: icon.node.name.replace(/\s+/g, '') + 'Icon',
+    };
+  });
 
-  const iconSizes = [
-    { label: 'Small', value: fontSizes.large.toString() },
-    { label: 'Medium', value: fontSizes.extraLarge2.toString() },
-    { label: 'Large', value: fontSizes.extraLarge4.toString() },
-  ];
-  const { addToast } = useToast();
+  const categories = iconsQuery
+    .map(icon => icon.node.absolutePath.split('/').splice(-2, 1).toString())
+    .filter(unique)
+    .sort();
+
+  const grouped = groupBy(
+    categoriesMap,
+    (category: { category: string; name: string }) => category.category,
+  );
+
   const filteredIcons = React.useMemo(() => {
+    const isCategorySelected = category !== null && category?.value !== '';
     const iconEntries = Object.entries(props.icons);
+    const filteredOnCategory =
+      isCategorySelected &&
+      iconEntries.filter(icon => {
+        let k = grouped.get(category.value);
+        return k.some(l => l.name === icon[0]);
+      });
+    const returnIcons = isCategorySelected ? filteredOnCategory : iconEntries;
     if (filterString === '') {
-      return iconEntries;
+      return returnIcons;
     }
 
-    return matchSorter(iconEntries, filterString, { keys: ['0'] });
-  }, [filterString, props.icons]);
+    return matchSorter(returnIcons, filterString, { keys: ['0'] });
+  }, [filterString, props.icons, category, grouped]);
 
   const noHits = filteredIcons.length === 0;
   const feedbackText =
@@ -60,51 +111,52 @@ const IconList: React.FC<IconListProps> = props => {
     });
   };
 
-  const iconsQuery = useGetIcons();
-  const unique = (value, index, self) => {
-    return self.indexOf(value) === index;
-  };
-
-  const categories = iconsQuery
-    .map(icon => icon.node.absolutePath.split('/').splice(-2, 1).toString())
-    .filter(unique)
-    .sort();
-
   return (
     <div>
-      <TextField
-        label="Søk etter ikon"
-        feedback={feedbackText}
-        variant={noHits ? 'error' : 'info'}
-        value={filterString}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setFilterString(e.target.value)
-        }
-        prepend={<SearchIcon />}
-      />
+      <Heading4 as="div" style={{ marginBottom: '1rem' }}>
+        Filter
+      </Heading4>
+      <GridContainer spacing="medium">
+        <GridItem small={12} medium={6}>
+          <TextField
+            label="Søk etter ikon"
+            feedback={feedbackText}
+            variant={noHits ? 'error' : 'info'}
+            value={filterString}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setFilterString(e.target.value)
+            }
+            prepend={<SearchIcon />}
+          />
+        </GridItem>
+        <GridItem small={6} medium={3}>
+          <Dropdown
+            items={ICON_SIZES}
+            value={iconSize?.value}
+            onChange={e => setIconSize(e)}
+            label="Ikonstørrelse"
+          />
+        </GridItem>
+        <GridItem small={6} medium={3}>
+          <Dropdown
+            label="Kategori"
+            items={categories}
+            value={category?.value}
+            onChange={e => setCategory(e)}
+            clearable
+          />
+        </GridItem>
+      </GridContainer>
       {!noHits && (
         <>
-          <Heading4>Filter</Heading4>
           <div style={{ display: 'flex' }}>
             <Switch
               checked={isContrast}
               onChange={() => setContrast(prev => !prev)}
+              style={{ margin: '1rem 0' }}
             >
               Kontrast
             </Switch>
-            {/* <Dropdown
-              items={iconSizes}
-              value={iconSize?.value}
-              onChange={e => setIconSize(e)}
-              label="Ikonstørrelse"
-              style={{ width: '10rem' }}
-            ></Dropdown>
-            <Dropdown
-              style={{ width: '10rem' }}
-              label="Kategori"
-              items={categories}
-              clearable
-            /> */}
           </div>
           <ul className="icon-list">
             {filteredIcons.map(([iconName, Icon]: any) => (
