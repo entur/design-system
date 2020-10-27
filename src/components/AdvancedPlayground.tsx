71c1eb1bf0205c9ea3fe2c17b8dfef7ea0af5eec
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-10-27 19:51:09 +0100

    feat: add first iteration of advanced playground component

---
+++ src/components/AdvancedPlayground.tsx
@@ -0,0 +1,222 @@
+import { Dropdown } from '@entur/dropdown';
+import {
+  SegmentedChoice,
+  SegmentedControl,
+  Switch,
+  TextField,
+} from '@entur/form';
+import { Heading5 } from '@entur/typography';
+import classNames from 'classnames';
+import React from 'react';
+import './AdvancedPlayground.scss';
+
+type BooleanVariant = {
+  name: string;
+  defaultValue: true | false;
+  type: 'boolean';
+  label?: string;
+};
+
+type MultiVariant = {
+  name: string;
+  options: string[];
+  defaultValue: string;
+  type: 'dropdown' | 'segmented';
+  label?: string;
+};
+type StringVariant = {
+  name: string;
+  defaultValue: string;
+  type: 'string';
+  label?: string;
+};
+
+type AdvancedProps = BooleanVariant | MultiVariant | StringVariant;
+
+type AdvancedPlaygroundProps = {
+  children: React.ReactElement;
+  props: AdvancedProps[];
+};
+
+function capitalize(s: string) {
+  return s && s[0].toUpperCase() + s.slice(1);
+}
+
+export const AdvancedPlayground: React.FC<AdvancedPlaygroundProps> = ({
+  children,
+  props,
+}) => {
+  const [isContrast, setContrast] = React.useState(false);
+  const [propState, setPropState] = React.useState(
+    props.map(p => {
+      let name = p.name;
+      let df = p.defaultValue;
+      return { [name]: { [name]: df } };
+    }),
+  );
+
+  const handleChange = (name, value) => {
+    let prevState = propState;
+    setPropState(
+      prevState.map(prev => {
+        return Object.keys(prev)[0] === name
+          ? { [name]: { [name]: value } }
+          : prev;
+      }),
+    );
+  };
+
+  return (
+    <div className="eds-advanced__wrapper">
+      <div
+        className={classNames('eds-advanced__playground', {
+          'eds-contrast': isContrast,
+        })}
+      >
+        {React.cloneElement(
+          children,
+          Object.assign(
+            ...propState.map(props => props[Object.keys(props)[0]]),
+          ),
+        )}
+      </div>
+      <Switch
+        className={classNames('eds-advanced__contrast-switch', {
+          'eds-contrast': isContrast,
+        })}
+        name="contrast"
+        checked={isContrast}
+        onChange={() => setContrast(prev => !prev)}
+      >
+        Contrast
+      </Switch>
+      <div className="eds-advanced__props">
+        <Heading5 as="div" margin="bottom">
+          Props
+        </Heading5>
+        {props.map(p => {
+          if (p.type === 'string') {
+            return (
+              <TextController
+                key={p.name}
+                name={p.name}
+                label={p.label ? p.label : p.name}
+                value={p.defaultValue}
+                setPlaygroundState={handleChange}
+              />
+            );
+          } else if (p.type === 'boolean') {
+            return (
+              <SwitchController
+                key={p.name}
+                name={p.name}
+                label={p.label ? p.label : capitalize(p.name)}
+                value={p.defaultValue}
+                setPlaygroundState={handleChange}
+              />
+            );
+          } else if (p.type === 'dropdown') {
+            return (
+              <DropdownController
+                key={p.name}
+                name={p.name}
+                items={p.options}
+                label={p.label ? p.label : capitalize(p.name)}
+                value={p.defaultValue}
+                setPlaygroundState={handleChange}
+              />
+            );
+          } else if (p.type === 'segmented') {
+            return (
+              <SegmentedController
+                key={p.name}
+                name={p.name}
+                options={p.options}
+                value={p.defaultValue}
+                label={p.label ? p.label : capitalize(p.name)}
+                setPlaygroundState={handleChange}
+              />
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
+};
+
+const DropdownController = ({
+  name,
+  label,
+  items,
+  value,
+  setPlaygroundState,
+}) => {
+  const [item, setItem] = React.useState(value);
+  React.useEffect(() => {
+    setPlaygroundState(name, item);
+  }, [item]);
+  return (
+    <Dropdown
+      label={label}
+      items={items}
+      value={item}
+      onChange={e => setItem(e?.value)}
+    ></Dropdown>
+  );
+};
+
+const SwitchController = ({ name, label, value, setPlaygroundState }) => {
+  const [checked, setChecked] = React.useState(value);
+
+  React.useEffect(() => {
+    setPlaygroundState(name, checked);
+  }, [checked]);
+
+  return (
+    <Switch
+      value={checked}
+      checked={checked}
+      onChange={e => setChecked(e.target.checked)}
+    >
+      <div className="eds-advanced__switch-label">{label}</div>
+    </Switch>
+  );
+};
+
+const TextController = ({ name, label, value, setPlaygroundState }) => {
+  const [state, setState] = React.useState(value);
+  React.useEffect(() => {
+    setPlaygroundState(name, state);
+  }, [state]);
+  return (
+    <TextField
+      label={label}
+      value={state}
+      onChange={e => setState(e.target.value)}
+    ></TextField>
+  );
+};
+
+const SegmentedController = ({
+  name,
+  label,
+  value,
+  options,
+  setPlaygroundState,
+}) => {
+  const [state, setstate] = React.useState(value);
+  React.useEffect(() => {
+    setPlaygroundState(name, state);
+  }, [state]);
+  return (
+    <SegmentedControl
+      label={label}
+      onChange={selectedValue => setstate(selectedValue)}
+      selectedValue={state}
+    >
+      {options.map(option => (
+        <SegmentedChoice value={option}>{option}</SegmentedChoice>
+      ))}
+    </SegmentedControl>
+  );
+};
