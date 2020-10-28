66a7b93769ec5e490d5222f23bcd17d2daaa9e21
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-10-28 22:54:37 +0100

    docs: add advanced playground to button, fab and datepicker

---
+++ content/components/skjemaelementer/props.tsx
@@ -0,0 +1,26 @@
+export const datepicker = [
+  {
+    name: 'label',
+    defaultValue: 'Velg utreisedato',
+    type: 'string',
+    label: 'Label',
+  },
+  {
+    name: 'placeholder',
+    defaultValue: 'Velg en dato',
+    type: 'string',
+    label: 'Placeholder',
+  },
+  {
+    name: 'variant',
+    options: ['success', 'error', 'warning', 'info'],
+    defaultValue: null,
+    type: 'dropdown',
+  },
+  {
+    name: 'feedback',
+    defaultValue: '',
+    type: 'string',
+    label: 'Feedback',
+  },
+];
