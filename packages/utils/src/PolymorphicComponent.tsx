53846421f66f4b6193238d0d6cced8b09658d3b6
Nicolai Fredriksen <nico-fre@hotmail.com>
2020-12-01 16:34:17 +0100

    feat: add polymorphic component dependecy as export
    affects: @entur/utils
    
    as several packages will depend on it, it'll save time when updating it, and generally ensures
    maintainability

---
+++ packages/utils/src/PolymorphicComponent.tsx
@@ -0,0 +1,4 @@
+// Direct export from dependency, as all packages are dependant on utils
+// Avoids adding the dependency to several packages
+import { Box, PolymorphicComponentProps } from 'react-polymorphic-box';
+export { Box, PolymorphicComponentProps };
