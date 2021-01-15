bcd1045a92784102c34247927662bd1104f93d4f
Nicolai Fredriksen <nico-fre@hotmail.com>
2021-01-15 10:45:55 +0100

    fix: fix searchbar shortcut in code editor

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -28,7 +28,10 @@ export const SearchBar: React.FC<SearchBarProps> = ({
       const hasFocus = inputRef.current === document.activeElement;
       switch (e.key) {
         case '/':
-          if (!hasFocus) {
+          if (
+            !hasFocus &&
+            !document.activeElement?.className.includes('code-editor')
+          ) {
             previousFocusRef.current = document.activeElement as HTMLElement;
             e.stopPropagation();
             inputRef.current!.focus(); // inputRef will always be set
