bc1cfff98c11082bad99b438bc720b7f8085340b
Nicolai Fredriksen <nico-fre@hotmail.com>
2021-01-29 09:21:59 +0100

    fix(nativedatepicker): fix label position for async values
    affects: @entur/datepicker

--- packages/datepicker/src/NativeDatePicker.tsx
+++ packages/datepicker/src/NativeDatePicker.tsx
@@ -69,12 +69,15 @@ type NativeDatePickerBaseProps = {
   onChange?: any;
   variant?: VariantType;
   id: string;
-};
+} & React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
 
 const NativeDatePickerBase = React.forwardRef<
   HTMLInputElement,
   NativeDatePickerBaseProps
->(({ onChange, id, variant, ...rest }, ref) => {
+>(({ onChange, id, variant, value, ...rest }, ref) => {
   const contextVariant = useVariant();
   const currentVariant = variant || contextVariant;
   const {
@@ -85,6 +88,14 @@ const NativeDatePickerBase = React.forwardRef<
   useOnMount(() => {
     setFiller && !isDatepickerFilled && setFiller(true);
   });
+
+  React.useEffect(() => {
+    if (value) {
+      setFiller && !isDatepickerFilled && setFiller(true);
+    } else {
+      setFiller && isDatepickerFilled && setFiller(false);
+    }
+  }, [value]);
 
   const handleChange = (event: any) => {
     if (isFilled(event.target)) {
@@ -104,6 +115,7 @@ const NativeDatePickerBase = React.forwardRef<
       className="eds-form-control eds-native-date-picker"
       onChange={handleChange}
       id={id}
+      value={value}
       {...rest}
     />
   );
