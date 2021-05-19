df510e346b0e17ef6f05596185c342ba971a98f5
NicolaiF <nico-fre@hotmail.com>
2021-05-19 08:52:26 +0200

    docs: improve multiselect documentation

--- content/components/skjemaelementer/dropdownItems.ts
+++ content/components/skjemaelementer/dropdownItems.ts
@@ -26,3 +26,17 @@ export default [
     icons: [TramIcon, BusIcon, TrainIcon],
   },
 ];
+
+export const FILTERS = [
+  { value: 'bus', label: 'Buss' },
+  { value: 'tram', label: 'Trikk' },
+  { value: 'rail', label: 'Tog' },
+  { value: 'metro', label: 'T-bane' },
+  { value: 'water', label: 'Ferge' },
+  { value: 'air', label: 'Fly' },
+  { value: 'flytog', label: 'Flytog' },
+  {
+    value: 'flybuss',
+    label: 'Flybuss',
+  },
+];
