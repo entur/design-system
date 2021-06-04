7e356d4dabeed0863f585f4d12384f9308b7d571
NicolaiF <nico-fre@hotmail.com>
2021-06-04 13:36:50 +0200

    fix(input group label): spread additional props on label element
    affects: @entur/form
    
    internally used component

--- packages/form/src/InputGroupLabel.tsx
+++ packages/form/src/InputGroupLabel.tsx
@@ -3,21 +3,27 @@ import React from 'react';
 import { useInputGroupContext } from './InputGroupContext';
 import './InputGroupLabel.scss';
 
-export const InputGroupLabel: React.FC<{
-  label?: React.ReactNode;
-  required?: boolean;
-  labelTooltip?: string;
-  labelId: string;
-  staticAnimation?: boolean;
-}> = ({ label, required, labelId, staticAnimation = false }) => {
+export const InputGroupLabel: React.FC<
+  {
+    label?: React.ReactNode;
+    required?: boolean;
+    labelTooltip?: string;
+    labelId: string;
+    staticAnimation?: boolean;
+  } & React.DetailedHTMLProps<
+    React.LabelHTMLAttributes<HTMLLabelElement>,
+    HTMLLabelElement
+  >
+> = ({ label, required, labelId, staticAnimation = false, ...rest }) => {
   const { isFilled } = useInputGroupContext();
   const filler = staticAnimation || isFilled;
   return (
     <label
-      className={classNames({
+      className={classNames(rest.className, {
         'eds-input-group-label-wrapper--filled': filler,
       })}
       id={labelId}
+      {...rest}
     >
       <span
         className={classNames('eds-input-group__label', {
