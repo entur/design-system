652dfc7e176e02ff8bcd5df20ef125decba65318
NicolaiF <nico-fre@hotmail.com>
2021-08-30 12:29:37 +0200

    fix: fix lint warnings in test
    affects: @entur/expand

--- packages/expand/src/Accordion.test.tsx
+++ packages/expand/src/Accordion.test.tsx
@@ -11,82 +11,82 @@ test('renders a single accordion item', async () => {
   const { getByTestId, queryByText } = render(
     <Accordion>
       <AccordionItem title="Trains" data-testid="trains-button">
-        Trains go "choo choo"
+        Trains go choo choo
       </AccordionItem>
     </Accordion>,
   );
 
-  expect(queryByText('Trains go "choo choo"')).not.toBeInTheDocument();
+  expect(queryByText('Trains go choo choo')).not.toBeInTheDocument();
 
   fireEvent.click(getByTestId('trains-button'));
 
-  expect(queryByText('Trains go "choo choo"')).toBeInTheDocument();
+  expect(queryByText('Trains go choo choo')).toBeInTheDocument();
 
   fireEvent.click(getByTestId('trains-button'));
 
-  expect(queryByText('Trains go "choo choo"')).not.toBeInTheDocument();
+  expect(queryByText('Trains go choo choo')).not.toBeInTheDocument();
 });
 
 test('renders a group of accordion items that can be opened and closed', () => {
   const { getByTestId, queryByText } = render(
     <Accordion>
       <AccordionItem title="Trains" data-testid="trains-button">
-        Trains go "choo choo"
+        Trains go choo choo
       </AccordionItem>
       <AccordionItem title="Boats" data-testid="boats-button">
         Boats float
       </AccordionItem>
       <AccordionItem title="Buses" data-testid="buses-button">
-        Buses go "vroom vroom"
+        Buses go vroom vroom
       </AccordionItem>
     </Accordion>,
   );
 
-  expect(queryByText('Trains go "choo choo"')).not.toBeInTheDocument();
+  expect(queryByText('Trains go choo choo')).not.toBeInTheDocument();
   expect(queryByText('Boats float')).not.toBeInTheDocument();
-  expect(queryByText('Buses go "vroom vroom"')).not.toBeInTheDocument();
+  expect(queryByText('Buses go vroom vroom')).not.toBeInTheDocument();
 
   fireEvent.click(getByTestId('boats-button'));
 
-  expect(queryByText('Trains go "choo choo"')).not.toBeInTheDocument();
+  expect(queryByText('Trains go choo choo')).not.toBeInTheDocument();
   expect(queryByText('Boats float')).toBeInTheDocument();
-  expect(queryByText('Buses go "vroom vroom"')).not.toBeInTheDocument();
+  expect(queryByText('Buses go vroom vroom')).not.toBeInTheDocument();
 
   fireEvent.click(getByTestId('trains-button'));
 
-  expect(queryByText('Trains go "choo choo"')).toBeInTheDocument();
+  expect(queryByText('Trains go choo choo')).toBeInTheDocument();
   expect(queryByText('Boats float')).not.toBeInTheDocument();
-  expect(queryByText('Buses go "vroom vroom"')).not.toBeInTheDocument();
+  expect(queryByText('Buses go vroom vroom')).not.toBeInTheDocument();
 
   fireEvent.click(getByTestId('trains-button'));
 
-  expect(queryByText('Trains go "choo choo"')).not.toBeInTheDocument();
+  expect(queryByText('Trains go choo choo')).not.toBeInTheDocument();
   expect(queryByText('Boats float')).not.toBeInTheDocument();
-  expect(queryByText('Buses go "vroom vroom"')).not.toBeInTheDocument();
+  expect(queryByText('Buses go vroom vroom')).not.toBeInTheDocument();
 });
 
 test('works with the defaultOpen option', async () => {
   const { getByTestId, queryByText } = render(
     <Accordion>
       <AccordionItem title="Trains" defaultOpen data-testid="trains-button">
-        Trains go "choo choo"
+        Trains go choo choo
       </AccordionItem>
       <AccordionItem title="Boats" data-testid="boats-button">
         Boats float
       </AccordionItem>
       <AccordionItem title="Buses" data-testid="buses-button">
-        Buses go "vroom vroom"
+        Buses go vroom vroom
       </AccordionItem>
     </Accordion>,
   );
 
-  expect(queryByText('Trains go "choo choo"')).toBeInTheDocument();
+  expect(queryByText('Trains go choo choo')).toBeInTheDocument();
   expect(queryByText('Boats float')).not.toBeInTheDocument();
-  expect(queryByText('Buses go "vroom vroom"')).not.toBeInTheDocument();
+  expect(queryByText('Buses go vroom vroom')).not.toBeInTheDocument();
 
   fireEvent.click(getByTestId('boats-button'));
 
-  expect(queryByText('Trains go "choo choo"')).not.toBeInTheDocument();
+  expect(queryByText('Trains go choo choo')).not.toBeInTheDocument();
   expect(queryByText('Boats float')).toBeInTheDocument();
-  expect(queryByText('Buses go "vroom vroom"')).not.toBeInTheDocument();
+  expect(queryByText('Buses go vroom vroom')).not.toBeInTheDocument();
 });
