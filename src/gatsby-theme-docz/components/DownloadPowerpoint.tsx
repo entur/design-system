adeb1de17c2a9f3a14b731fcd8efb61a16fe6e5e
NicolaiF <nico-fre@hotmail.com>
2021-09-20 13:08:13 +0200

    feat: add powerpoint page

---
+++ src/gatsby-theme-docz/components/DownloadPowerpoint.tsx
@@ -0,0 +1,30 @@
+import { TertiaryButton } from '@entur/button';
+import { graphql, useStaticQuery } from 'gatsby';
+import React from 'react';
+import { DownloadIcon } from '@entur/icons';
+
+export const DownloadPowerpoint = () => {
+  const query = useStaticQuery(graphql`
+    query PowerpointFile {
+      file(
+        sourceInstanceName: { eq: "downloads" }
+        name: { eq: "Entur_Powerpointmal" }
+      ) {
+        extension
+        name
+        publicURL
+      }
+    }
+  `);
+  return (
+    <TertiaryButton
+      as="a"
+      href={query.file.publicURL}
+      download
+      style={{ marginBottom: '2.5rem' }}
+    >
+      <DownloadIcon />
+      {query.file.name}.{query.file.extension}
+    </TertiaryButton>
+  );
+};
