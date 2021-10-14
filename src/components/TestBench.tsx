48dd124441fb7604e3e68b91f095ef90169d5942
NicolaiF <nico-fre@hotmail.com>
2021-10-14 13:21:25 +0200

    fix: add basic testbench file

---
+++ src/components/TestBench.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { PrimaryButton } from '@entur/button/dist';
+
+export const TestBench = () => {
+  return (
+    <div>
+      <PrimaryButton>Klikk</PrimaryButton>
+    </div>
+  );
+};
