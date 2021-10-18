67427400a674e19b02f723116c1c38876bfde250
NicolaiF <nico-fre@hotmail.com>
2021-10-18 10:10:34 +0200

    fix: only one window in playroom

--- playroom.config.js
+++ playroom.config.js
@@ -5,7 +5,7 @@ module.exports = {
   // Optional:
   title: 'TestBench',
   frameComponent: './playroom/FrameComponent.tsx',
-  widths: [768, 1300],
+  widths: [1300],
   port: 9000,
   openBrowser: true,
   paramType: 'search', // default is 'hash'
