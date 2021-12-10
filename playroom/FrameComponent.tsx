269bb213e30d83c5406682d3e78468965a829540
NicolaiF <nico-fre@hotmail.com>
2021-12-10 15:13:15 +0100

    fix: add contrast switch to playroom

--- playroom/FrameComponent.tsx
+++ playroom/FrameComponent.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { TestBench } from '../src/components/TestBench';
+import { Label } from '@entur/typography';
+import { Switch } from '@entur/form';
+import { Contrast } from '@entur/layout';
 
 const FrameComponent = ({ children }) => {
+  const [contrast, setContrast] = React.useState(false);
+  const Element = contrast ? Contrast : 'div';
   return (
-    <div style={{ margin: '2rem' }}>
-      <TestBench />
+    <Element>
+      <div style={{ paddingBottom: '1rem' }}>
+        <Label>
+          Kontrast
+          <Switch checked={contrast} onChange={() => setContrast(!contrast)} />
+        </Label>
+      </div>
+      <div style={{ margin: '2rem' }}>
+        <TestBench />
+      </div>
       {children}
-    </div>
+    </Element>
   );
 };
 
