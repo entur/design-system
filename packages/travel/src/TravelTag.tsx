42a191a2962f34696f2cfcb42c943e60c2738f4b
Magnus Rand <magnus.rand@gmail.com>
2021-12-15 12:33:32 +0100

    feat(travel tag): add transport icon automatically based on transport prop
    affects: @entur/travel
    
    BREAKING CHANGE:
    If both an icon and transport prop is provided in previous code, you will now get two icos. Remove
    the child Icon to fix

--- packages/travel/src/TravelTag.tsx
+++ packages/travel/src/TravelTag.tsx
@@ -5,23 +5,35 @@ import {
   ValidationInfoIcon,
   ValidationErrorIcon,
   ValidationExclamationIcon,
+  BicycleIcon,
+  BusIcon,
+  FerryIcon,
+  PlaneIcon,
+  ScooterIcon,
+  SubwayIcon,
+  TrainIcon,
+  TramIcon,
+  WalkingIcon,
+  CarIcon,
+  TaxiIcon,
+  CarferryIcon,
 } from '@entur/icons';
 import './TravelTag.scss';
 
 export type TravelTagProps = {
-  /** Callback som kalles for når man skal lukke TravelTagen
+  /** Callback som kalles for når man skal lukke TravelTag-en
    * @default undefined
    */
   onClose?: () => void;
-  /** Innholdet til TravelTagen */
-  children: React.ReactNode;
+  /** Innholdet inne i TravelTag-en */
+  children?: React.ReactNode;
   /**Ekstra klassenavn */
   className?: string;
   /** Legger til et Valideringsikon i TravelTagen for å signalisere avvik, informasjon e.l.
    * @default "none"
    */
   alert?: 'none' | 'error' | 'warning' | 'info';
-  /** Legger til farger tilpasset valgt transportmiddel */
+  /** Legger til farge og ikon tilpasset valgt transportmiddel */
   transport?:
     | 'bus'
     | 'metro'
@@ -29,10 +41,12 @@ export type TravelTagProps = {
     | 'tram'
     | 'rail'
     | 'water'
+    | 'carferry'
     | 'bike'
     | 'scooter'
     | 'foot'
-    | 'car';
+    | 'car'
+    | 'taxi';
   /** Element ved siden av eller under TravelTag.  */
   label?: React.ReactNode;
   /** Posisjonen til label-en i forhold til TravelTag-en
@@ -56,6 +70,7 @@ export const TravelTag: React.FC<TravelTagProps> = ({
 }) => {
   const isClosable = onClose ? true : false;
   const numberOfChildren = React.Children.count(children);
+  const TransportIcon = modeCalc(transport) ?? <></>;
 
   const TravelTagWithoutLabel: JSX.Element = (
     <div
@@ -63,12 +78,14 @@ export const TravelTag: React.FC<TravelTagProps> = ({
         'eds-travel-tag--closable': isClosable,
         'eds-travel-tag--alert': alert !== 'none',
         'eds-travel-tag--alert--error': alert === 'error',
-        'eds-travel-tag--icon-and-text': numberOfChildren > 1,
+        'eds-travel-tag--icon-and-text':
+          numberOfChildren > 1 || (transport && numberOfChildren > 0),
         [`eds-travel-tag--transport-${transport}`]: transport,
         className,
       })}
       {...rest}
     >
+      {transport && TransportIcon}
       {children}
       {isClosable && (
         <button onClick={onClose} className="eds-travel-tag__close-button">
@@ -117,4 +134,35 @@ export const TravelTag: React.FC<TravelTagProps> = ({
   }
 
   return TravelTagWithoutLabel;
+};
+
+const modeCalc = (mode: string | undefined) => {
+  switch (mode) {
+    case 'bus':
+      return <BusIcon />;
+    case 'metro':
+      return <SubwayIcon />;
+    case 'air':
+      return <PlaneIcon />;
+    case 'tram':
+      return <TramIcon />;
+    case 'rail':
+      return <TrainIcon />;
+    case 'water':
+      return <FerryIcon />;
+    case 'carferry':
+      return <CarferryIcon />;
+    case 'bike':
+      return <BicycleIcon />;
+    case 'scooter':
+      return <ScooterIcon />;
+    case 'foot':
+      return <WalkingIcon />;
+    case 'car':
+      return <CarIcon />;
+    case 'taxi':
+      return <TaxiIcon />;
+    default:
+      return <></>;
+  }
 };
