d959e53d6b50a7cd867c2d49023cffdb26f710f8
Magnus Rand <magnus.rand@gmail.com>
2022-03-24 14:12:21 +0100

    Add postcss with postcss-preset-env

--- dts.config.js
+++ dts.config.js
@@ -1,8 +1,18 @@
 const sass = require('rollup-plugin-sass');
+const postcss = require('postcss');
+const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
   rollup(config) {
-    config.plugins.unshift(sass({ output: 'dist/styles.css' }));
+    config.plugins.unshift(
+      sass({
+        output: 'dist/styles.css',
+        processor: css =>
+          postcss([postcssPresetEnv])
+            .process(css, { from: undefined })
+            .then(result => result.css),
+      }),
+    );
     return config;
   },
 };
