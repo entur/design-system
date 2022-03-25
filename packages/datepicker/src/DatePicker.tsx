6c9c8636a4eec59b86db928aceb9e4e01c1fa07e
Magnus Rand <magnus.rand@gmail.com>
2022-03-25 15:43:45 +0100

    feat(datepicker): add custom header to datepicker
    affects: @entur/datepicker
    
    The datepicker now uses a custom header component in the calendar insted of the built-in header.
    This fixes a bug in Safari where the month selection arrows were invisible
    
    BREAKING CHANGE:
    Complete rewrite of component may have introduced unnoticed bugs

--- packages/datepicker/src/DatePicker.tsx
+++ packages/datepicker/src/DatePicker.tsx
@@ -1,22 +1,17 @@
 import React, { useState } from 'react';
 import {
   default as ReactDatepicker,
+  ReactDatePickerCustomHeaderProps,
   ReactDatePickerProps,
   registerLocale,
 } from 'react-datepicker';
-import classNames from 'classnames';
-import {
-  BaseFormControl,
-  useInputGroupContext,
-  VariantType,
-} from '@entur/form';
-import { CalendarIcon } from '@entur/icons';
-import { parse, isSameDay } from 'date-fns';
+import { VariantType } from '@entur/form';
 import { nb } from 'date-fns/locale';
-import './DatePicker.scss';
-import { Tooltip } from '@entur/tooltip';
 import { useRandomId } from '@entur/utils';
-import * as Popper from '@popperjs/core';
+import './DatePicker.scss';
+import { IconButton } from '@entur/button';
+import { LeftArrowIcon, RightArrowIcon } from '@entur/icons';
+import { Heading3 } from '@entur/typography';
 
 registerLocale('nb', nb);
 
@@ -33,7 +28,7 @@ export type DatePickerProps = {
    */
   onChangeInput?: (value: string) => void;
   /** Placeholder om ingen dato er valgt
-   * @default "dd.mm.yyyy"
+   * @default "dd/mm/yyyy"
    */
   placeholder?: string;
   /** Ekstra klassenavn */
@@ -78,292 +73,92 @@ export type DatePickerProps = {
   hideCalendar?: boolean;
   // For testing
   'data-cy'?: any;
-} & Omit<
-  ReactDatePickerProps,
-  'selected' | 'customInput' | 'onChangeRaw' | 'onChange'
->;
-const POPPER_MODIFIERS: Popper.StrictModifiers[] = [
-  {
-    name: 'offset',
-    enabled: true,
-    options: {
-      offset: [0, 0],
-    },
-  },
-];
+} & Omit<ReactDatePickerProps, 'selected' | 'customInput' | 'onChangeRaw'>;
 
 export const DatePicker = React.forwardRef<HTMLInputElement, DatePickerProps>(
-  (
-    {
-      selectedDate = null,
-      onChange,
-      onChangeInput,
-      placeholder = 'dd.mm.yyyy',
-      className,
-      style,
-      readOnly,
-      disableLabelAnimation = false,
-      locale = 'nb',
-      prepend,
-      disabled,
-      variant,
-      validationVariant = 'error',
-      feedback,
-      validationFeedback = 'Ugyldig dato',
-      label,
-      hideCalendarButton = false,
-      hideCalendar = false,
-      hideValidationFeedback = false,
-      id,
-      ...rest
-    },
-    ref,
-  ) => {
-    const [showValidationFeedback, setShowValidationFeedback] = useState(false);
-    const [currentValue, setCurrentValue] = useState('');
-    const [lastValidValue, setLastValidValue] = useState('');
-
+  () => {
     const datepickerId = useRandomId('eds-datepicker');
-    const { isFilled, setFilled } = useInputGroupContext();
 
-    React.useEffect(() => {
-      setFilled(!!selectedDate);
-      handleChange(selectedDate, undefined);
-    }, [selectedDate, setFilled, isFilled]);
-
-    const handleChange = (
-      date: Date | [Date | null, Date | null] | /* for selectsRange */ null,
-      event: React.SyntheticEvent<any> | undefined,
-    ) => {
-      // The return value from reactDatePicker can potentially be a range of dates, this however is not supported.
-      // To circumvent this we create simply pick the first selected value in that case.
-      const newDate = [date].flat()[0];
-      setFilled(!!newDate);
-      const dateString = newDate?.toLocaleDateString('no-NO', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-      });
-      setCurrentValue(dateString ?? '');
-      setLastValidValue(dateString ?? '');
-      if (onChange) {
-        onChange(newDate, event);
-      }
-    };
-
-    const handleChangeRaw = (event: React.FocusEvent<HTMLInputElement>) => {
-      setShowValidationFeedback(false);
-      const inputValue = event.target.value;
-      setCurrentValue(inputValue);
-      if (onChangeInput) onChangeInput(inputValue);
-    };
-
-    const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
-      validateInput(event.target.value);
-    };
-
-    const handleKeyDownInput = (
-      event: React.KeyboardEvent<HTMLInputElement>,
-    ) => {
-      if (event.key === 'Enter') {
-        validateInput(event.currentTarget.value);
-      }
-    };
-
-    const validateInput = (inputValue: string | undefined) => {
-      setShowValidationFeedback(false);
-      if (!inputValue) {
-        return;
-      }
-      const parsedDate = parse(inputValue, 'dd.MM.yyyy', new Date(), {
-        locale: nb,
-      });
-
-      const selectedDateIsTheSameAsParsedDateFromInput =
-        selectedDate && isSameDay(parsedDate, selectedDate);
-      if (selectedDateIsTheSameAsParsedDateFromInput) {
-        setLastValidValue(currentValue);
-      } else {
-        setShowValidationFeedback(true);
-        setCurrentValue(lastValidValue);
-      }
-    };
-
-    const displayedFeedback = (): string => {
-      if (feedback) return feedback;
-      if (!hideValidationFeedback && showValidationFeedback)
-        return validationFeedback;
-      return '';
-    };
-
-    const displayedVariant = (): string | undefined => {
-      if (feedback && variant) return variant;
-      if (!hideValidationFeedback && showValidationFeedback)
-        return validationVariant;
-      return undefined;
-    };
+    const [chosenDate, setChosenDate] = useState(new Date());
 
     return (
       <ReactDatepicker
-        className={classNames(className)}
-        calendarClassName="eds-datepicker__calender"
-        selected={selectedDate}
-        onChange={handleChange}
-        showWeekNumbers={true}
-        dateFormat={['dd.MM.yyyy', 'ddMMyyyy', 'dd/MM/yyyy']}
-        showPopperArrow={false}
-        placeholderText={placeholder}
-        readOnly={readOnly}
+        selected={chosenDate}
+        onChange={date => setChosenDate(date as Date)}
         id={datepickerId}
-        ariaLabelledBy={datepickerId}
-        disabled={disabled}
-        locale={locale}
-        popperModifiers={POPPER_MODIFIERS}
-        onBlur={handleBlur}
-        onChangeRaw={handleChangeRaw}
-        onCalendarOpen={() => setShowValidationFeedback(false)}
-        value={currentValue}
-        open={hideCalendar === true ? false : rest.open}
-        {...rest}
-        customInput={
-          <DatePickerInput
-            style={style}
-            readOnly={readOnly}
-            variant={displayedVariant()}
-            feedback={displayedFeedback()}
-            label={label}
-            disabled={disabled}
-            ref={ref}
-            data-cy={rest['data-cy']}
-            disableLabelAnimation={disableLabelAnimation}
-            prepend={prepend}
-            hideCalendarButton={hideCalendarButton}
-            inputId={id}
-            onKeyDownInput={handleKeyDownInput}
-            onBlurInput={handleBlur}
-            onChangeInput={onChangeInput}
+        showWeekNumbers
+        showPopperArrow={false}
+        renderCustomHeader={({
+          date,
+          changeYear,
+          changeMonth,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled,
+        }) => (
+          <DatePickerHeader
+            date={date}
+            changeYear={changeYear}
+            changeMonth={changeMonth}
+            increaseMonth={increaseMonth}
+            decreaseMonth={decreaseMonth}
+            prevMonthButtonDisabled={prevMonthButtonDisabled}
+            nextMonthButtonDisabled={nextMonthButtonDisabled}
           />
-        }
+        )}
       />
     );
   },
 );
 
-type DatePickerInputProps = {
-  onClick?: any;
-  value?: any;
-  disableLabelAnimation?: boolean;
-  className?: string;
-  hideCalendarButton?: boolean;
-  feedback?: string;
-  inputId?: string;
-  [key: string]: any;
-};
-// Props fra customInput i react-datepicker
-// value: inputValue,
-// onBlur: this.handleBlur,
-// onChange: this.handleChange,
-// onClick: this.onInputClick,
-// onFocus: this.handleFocus,
-// onKeyDown: this.onInputKeyDown,
-// id: this.props.id,
-// name: this.props.name,
-// autoFocus: this.props.autoFocus,
-// placeholder: this.props.placeholderText,
-// disabled: this.props.disabled,
-// autoComplete: this.props.autoComplete,
-// className: classnames(customInput.props.className, className),
-// title: this.props.title,
-// readOnly: this.props.readOnly,
-// required: this.props.required,
-// tabIndex: this.props.tabIndex,
-// "aria-describedby": this.props.ariaDescribedBy,
-// "aria-invalid": this.props.ariaInvalid,
-// "aria-labelledby": this.props.ariaLabelledBy,
-// "aria-required": this.props.ariaRequired,
+function getMonthList(locale = 'nb') {
+  const year = new Date().getFullYear(); // 2020
+  const monthList = Array(12).keys(); // an Array Iterator
+  const formatter = new Intl.DateTimeFormat(locale, {
+    month: 'long',
+  });
+  const getMonthName = (monthIndex: number) =>
+    formatter.format(new Date(year, monthIndex));
 
-const DatePickerInput = React.forwardRef<
-  HTMLInputElement,
-  DatePickerInputProps
->(
-  (
-    {
-      value,
-      onClick,
-      // Capture onFocus prop from react-datepicker, but doesn't use it
-      // eslint-disable-next-line
-      onFocus,
-      onKeyDown,
-      variant,
-      feedback,
-      style,
-      disableLabelAnimation,
-      disabled,
-      label,
-      readOnly,
-      id,
-      prepend,
-      className,
-      hideCalendarButton,
-      inputId,
-      onChangeInput,
-      onKeyDownInput,
-      onBlurInput,
-      ...rest
-    },
-    ref,
-  ) => {
-    return (
-      <span className={className} onBlur={onBlurInput}>
-        <BaseFormControl
-          style={style}
-          className="eds-datepicker__form-control"
-          readOnly={readOnly}
-          label={label}
-          labelId={id}
-          feedback={feedback}
-          variant={variant}
-          disabled={disabled}
-          disableLabelAnimation={disableLabelAnimation}
-          isFilled={!!value}
-          prepend={prepend}
-        >
-          <input
-            value={value}
-            onClick={onClick}
-            readOnly={readOnly}
-            disabled={disabled}
-            ref={ref}
-            // aria-labelledby={id}
-            className="eds-form-control"
-            id={inputId}
-            onKeyDown={onKeyDownInput}
-            onChange={onChangeInput}
-            {...rest}
-          />
-          {!hideCalendarButton && (
-            <Tooltip
-              placement="top"
-              content="Åpne kalender"
-              disableHoverListener={disabled}
-              disableFocusListener={disabled}
-            >
-              <button
-                className={classNames('eds-datepicker__calendar-button', {
-                  'eds-datepicker__calendar-button--open': true,
-                  'eds-datepicker__calendar-button--disabled': disabled,
-                })}
-                onKeyDown={onKeyDown}
-                onClick={onClick}
-                disabled={disabled}
-                type="button"
-              >
-                <CalendarIcon />
-              </button>
-            </Tooltip>
-          )}
-        </BaseFormControl>
-      </span>
-    );
-  },
-);
+  return Array.from(monthList, getMonthName);
+}
+
+const DatePickerHeader = ({
+  date,
+  decreaseMonth,
+  increaseMonth,
+  prevMonthButtonDisabled,
+  nextMonthButtonDisabled,
+}: Partial<ReactDatePickerCustomHeaderProps>) => {
+  const monthNames = getMonthList();
+  return (
+    <div
+      style={{
+        margin: '0 0.4rem',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <IconButton
+        style={{ marginRight: 'auto' }}
+        onClick={decreaseMonth}
+        disabled={prevMonthButtonDisabled}
+      >
+        <LeftArrowIcon />
+      </IconButton>
+      <Heading3 style={{ margin: '0 0.25rem' }}>
+        {monthNames[date?.getMonth() ?? 0]}
+      </Heading3>
+      <Heading3 style={{ margin: '0 0.25rem' }}>{date?.getFullYear()}</Heading3>
+
+      <IconButton
+        style={{ marginLeft: 'auto' }}
+        onClick={increaseMonth}
+        disabled={nextMonthButtonDisabled}
+      >
+        <RightArrowIcon />
+      </IconButton>
+    </div>
+  );
+};
