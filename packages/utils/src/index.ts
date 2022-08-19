c91dc65513dba6c53cee251366007cdd68b369b3
Magnus Rand <magnus.rand@gmail.com>
2022-08-19 16:51:48 +0200

    refactor(utils): move mergeRefs function into utils
    affects: @entur/utils
    
    This function allows you to have multiple refs on a single element

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,4 +2,5 @@ export * from './debounce';
 export * from './PolymorphicComponent';
 export * from './useRandomId';
 export * from './useOnMount';
+export * from './mergeRefs';
 export * from './warnAboutMissingStyles';
---
+++ packages/utils/src/mergeRefs.ts
@@ -0,0 +1,11 @@
+export const mergeRefs = <T extends HTMLElement>(
+  ...refs: React.MutableRefObject<T>[] | React.ForwardedRef<T>[]
+) => {
+  return (node: T) => {
+    for (const ref of refs) {
+      if (typeof ref === 'function') {
+        ref(node);
+      } else if (ref) ref.current = node;
+    }
+  };
+};
