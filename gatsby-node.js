2bed80d65f704380353347f7888345a231254302
Eirik Vågeskar <eirik.vageskar@protonmail.com>
2022-11-01 15:54:33 +0100

    chore: introduce deduping to the project
    Add dedupe check to linting
    
    Run `yarn dedupe`
    
    Get Docz to build again after deduping

--- gatsby-node.js
+++ gatsby-node.js
@@ -79,3 +79,32 @@ exports.sourceNodes = async ({ createNodeId, actions: { createNode } }) => {
     }),
   );
 };
+
+exports.onCreateBabelConfig = ({ actions }) => {
+  actions.setBabelPreset({
+    /*
+     * This preset is necessary to avoid the following error:
+     *
+     *       ERROR #98123  WEBPACK
+     *
+     *     Generating SSR bundle failed
+     *
+     *     /Users/eirikvageskar/design-system/node_modules/docz/dist/index.esm.js: It's not possible to compile spread arguments in `super()` without compiling classes.
+     *     Please add '@babel/plugin-transform-classes' to your Babel configuration.
+     *       63 |     Provider: (_a = class Provider extends Component {
+     *       64 |       constructor() {
+     *     > 65 |         super(...arguments);
+     *          |         ^^^^^^^^^^^^^^^^^^^
+     *       66 |         this.state = this.props.initial || initial || {};
+     *       67 |       }
+     *       68 |
+     *
+     *     File: ../node_modules/docz/dist/index.esm.js
+     *
+     * An alternative is to pin the @babel/plugin-transform-spread to 7.12.1:
+     * https://github.com/gatsbyjs/gatsby/issues/29326#issuecomment-773066125
+     */
+    name: '@babel/preset-env',
+    options: {},
+  });
+};
