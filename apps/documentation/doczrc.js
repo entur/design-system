a7b72afcd2e421569c54d9df83003edf76814839
Magnus Rand <magnus.rand@gmail.com>
2023-02-07 19:13:36 +0100

    chore: improve docz build time

--- apps/documentation/doczrc.js
+++ apps/documentation/doczrc.js
@@ -1,13 +1,20 @@
-const searchPath =
-  process.env.NODE_ENV === 'production'
-    ? '../../packages'
-    : '../../packages/*/src';
+import path from 'path';
+
 export default {
   typescript: true,
   title: 'Entur Designsystem',
   ignore: ['README.md', 'CHANGELOG.md', '../../packages/*/node_modules'],
   src: 'content',
   docgenConfig: {
-    searchPath: searchPath,
+    propFilter: prop => {
+      return prop.declarations[0].fileName.includes('@entur');
+    },
+    resolver: (exp, source) => {
+      var name = path.basename(source.fileName, path.extname(source.fileName));
+      return path.basename(name, path.extname(name));
+    },
+    searchPath: '../../node_modules/@entur',
   },
+  filterComponents: files =>
+    files.filter(filepath => /\/[A-Z]\w*.d.ts$/.test(filepath)),
 };
