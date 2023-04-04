a29cd4c9f8ac0115a48fac1ff85a072d3a3393d5
Magnus Rand <magnus.rand@gmail.com>
2023-04-04 10:24:39 +0200

    fix(timepicker): fix typings for selectedTime and onChange-props
    affects: @entur/datepicker, @entur/utils
    
    The type of onChange input is now based on selectedTime input
    
    BREAKING CHANGE:
    ref prop has changed name to inputProp

--- packages/datepicker/src/TimePicker/TimePicker.tsx
+++ packages/datepicker/src/TimePicker/TimePicker.tsx
@@ -5,7 +5,11 @@ import { I18nProvider, useLocale } from '@react-aria/i18n';
 import { useTimeFieldState } from '@react-stately/datepicker';
 import { getLocalTimeZone, now } from '@internationalized/date';
 
-import type { TimeValue, AriaTimeFieldProps } from '@react-types/datepicker';
+import type {
+  TimeValue,
+  AriaTimeFieldProps,
+  MappedTimeValue,
+} from '@react-types/datepicker';
 
 import { BaseFormControl, VariantType } from '@entur/form';
 import { useRandomId, mergeRefs } from '@entur/utils';
@@ -15,11 +19,11 @@ import { TimePickerArrowButton } from './TimePickerArrowButton';
 
 import './TimePicker.scss';
 
-export type TimePickerProps = {
+export type TimePickerProps<TimeType extends TimeValue> = {
   /** Den valgte tiden. Tid i '@internationalized/date'-pakkens format */
-  selectedTime: TimeValue | null;
+  selectedTime: TimeType | null;
   /** Kalles når tiden endres. Tid i '@internationalized/date'-pakkens format */
-  onChange: (value: TimeValue) => void;
+  onChange: (value: MappedTimeValue<TimeType> | null) => void;
   /** Label til TimePicker */
   label: string;
   /** Minutter som legges til eller trekkes fra ved klikk på pilene i TimePicker
@@ -48,124 +52,131 @@ export type TimePickerProps = {
   variant?: VariantType;
   labelTooltip?: React.ReactNode;
   disabled?: boolean;
+  inputRef?: React.ForwardedRef<HTMLDivElement>;
   /** Ekstra klassenavn */
   className?: string;
   style?: React.CSSProperties;
 } & Omit<
-  AriaTimeFieldProps<TimeValue>,
-  'onChange' | 'label' | 'hideTimeZone' | 'placeholder'
+  AriaTimeFieldProps<TimeType>,
+  | 'value'
+  | 'onChange'
+  | 'label'
+  | 'hideTimeZone'
+  | 'placeholder'
+  | 'minValue'
+  | 'maxValue'
 >;
 
-export const TimePicker = React.forwardRef<HTMLDivElement, TimePickerProps>(
-  (
-    {
-      selectedTime,
-      onChange,
-      disabled,
-      className,
-      style,
-      label,
-      labelTooltip,
-      feedback,
-      variant,
-      locale: customLocale,
-      showTimeZone,
-      minuteIncrementForArrowButtons = 30,
-      leftArrowButtonAriaLabel = `Trekk fra ${minuteIncrementForArrowButtons} minutter`,
-      rightArrowButtonAriaLabel = `Legg til ${minuteIncrementForArrowButtons} minutter`,
-      ...rest
-    },
-    ref,
-  ) => {
-    let { locale } = useLocale();
-    if (customLocale) locale = customLocale;
+export const TimePicker = <TimeType extends TimeValue>({
+  selectedTime,
+  onChange,
+  disabled,
+  className,
+  style,
+  label,
+  labelTooltip,
+  feedback,
+  variant,
+  locale: customLocale,
+  showTimeZone,
+  minuteIncrementForArrowButtons = 30,
+  leftArrowButtonAriaLabel = `Trekk fra ${minuteIncrementForArrowButtons} minutter`,
+  rightArrowButtonAriaLabel = `Legg til ${minuteIncrementForArrowButtons} minutter`,
+  inputRef,
+  ...rest
+}: TimePickerProps<TimeType>) => {
+  let { locale } = useLocale();
+  if (customLocale) locale = customLocale;
 
-    const state = useTimeFieldState({
-      onChange,
-      label: label,
-      locale,
-      value: selectedTime === null ? undefined : selectedTime,
-      hideTimeZone: !showTimeZone,
-      isDisabled: disabled,
-      ...rest,
-    });
-    const timeFieldRef = useRef(null);
-    const { labelProps, fieldProps } = useTimeField(
-      { ...rest, label: label },
-      state,
-      timeFieldRef,
-    );
-    const id = useRandomId('timepicker');
+  const state = useTimeFieldState({
+    // @ts-expect-error Time should be assignable to MappedTime<TimeType>
+    onChange,
+    label: label,
+    locale,
+    value: selectedTime === null ? undefined : selectedTime,
+    hideTimeZone: !showTimeZone,
+    isDisabled: disabled,
+    ...rest,
+  });
+  const timeFieldRef = useRef(null);
+  const { labelProps, fieldProps } = useTimeField<TimeType>(
+    { ...rest, label: label },
+    state,
+    timeFieldRef,
+  );
+  const id = useRandomId('timepicker');
 
-    const handleOnClickArrowButton = (minutes: number) => {
-      if (someSegmentIsUndefined) {
-        setTimeToNearestMinuteIncrement();
-      } else {
-        addMinutesToSelectedTime(minutes);
-      }
-    };
+  const handleOnClickArrowButton = (minutes: number) => {
+    if (someSegmentIsUndefined) {
+      setTimeToNearestMinuteIncrement();
+    } else {
+      addMinutesToSelectedTime(minutes);
+    }
+  };
 
-    const someSegmentIsUndefined = state.segments.some(
-      segment => segment.text === '––',
-    );
+  const someSegmentIsUndefined = state.segments.some(
+    segment => segment.text === '––',
+  );
 
-    const setTimeToNearestMinuteIncrement = () => {
-      const currentTime = now(getLocalTimeZone());
-      const roundedMinute =
-        Math.floor(currentTime.minute / minuteIncrementForArrowButtons) *
-        minuteIncrementForArrowButtons;
-      const newTime = currentTime.set({ minute: roundedMinute });
-      onChange(newTime);
-    };
+  const setTimeToNearestMinuteIncrement = () => {
+    const currentTime = now(getLocalTimeZone());
+    const roundedMinute =
+      Math.floor(currentTime.minute / minuteIncrementForArrowButtons) *
+      minuteIncrementForArrowButtons;
+    const newTime = currentTime.set({ minute: roundedMinute });
+    // @ts-expect-error Since this function is used when selectedTime is null,
+    // we can't guarantee newTime matching the type of selectedTime in the future.
+    // This might lead to a type issue.
+    onChange(newTime);
+  };
 
-    const addMinutesToSelectedTime = (minutes: number) => {
-      state.value &&
-        state.setValue(
-          state.value?.add({
-            minutes: minutes,
-          }),
-        );
-    };
+  const addMinutesToSelectedTime = (minutes: number) => {
+    state.value &&
+      state.setValue(
+        state.value?.add({
+          minutes: minutes,
+        }),
+      );
+  };
 
-    return (
-      <I18nProvider locale={locale}>
-        <div className={classNames(className, 'eds-timepicker__wrapper')}>
-          <TimePickerArrowButton
-            direction="left"
-            disabled={disabled}
-            aria-label={leftArrowButtonAriaLabel}
-            onClick={() =>
-              handleOnClickArrowButton(minuteIncrementForArrowButtons * -1)
-            }
-          />
-          <BaseFormControl
-            style={style}
-            className={'eds-timepicker'}
-            labelId={id}
-            label={label}
-            labelProps={{ ...labelProps }}
-            ref={mergeRefs(timeFieldRef, ref)}
-            disabled={disabled}
-            disableLabelAnimation
-            labelTooltip={labelTooltip}
-            {...fieldProps}
-            variant={variant}
-            feedback={feedback}
-          >
-            {state.segments.map((segment, i) => (
-              <FieldSegment segment={segment} state={state} key={i} />
-            ))}
-          </BaseFormControl>
-          <TimePickerArrowButton
-            direction="right"
-            disabled={disabled}
-            aria-label={rightArrowButtonAriaLabel}
-            onClick={() =>
-              handleOnClickArrowButton(minuteIncrementForArrowButtons)
-            }
-          />
-        </div>
-      </I18nProvider>
-    );
-  },
-);
+  return (
+    <I18nProvider locale={locale}>
+      <div className={classNames(className, 'eds-timepicker__wrapper')}>
+        <TimePickerArrowButton
+          direction="left"
+          disabled={disabled}
+          aria-label={leftArrowButtonAriaLabel}
+          onClick={() =>
+            handleOnClickArrowButton(minuteIncrementForArrowButtons * -1)
+          }
+        />
+        <BaseFormControl
+          style={style}
+          className={'eds-timepicker'}
+          labelId={id}
+          label={label}
+          labelProps={{ ...labelProps }}
+          ref={mergeRefs(timeFieldRef, inputRef)}
+          disabled={disabled}
+          disableLabelAnimation
+          labelTooltip={labelTooltip}
+          {...fieldProps}
+          variant={variant}
+          feedback={feedback}
+        >
+          {state.segments.map((segment, i) => (
+            <FieldSegment segment={segment} state={state} key={i} />
+          ))}
+        </BaseFormControl>
+        <TimePickerArrowButton
+          direction="right"
+          disabled={disabled}
+          aria-label={rightArrowButtonAriaLabel}
+          onClick={() =>
+            handleOnClickArrowButton(minuteIncrementForArrowButtons)
+          }
+        />
+      </div>
+    </I18nProvider>
+  );
+};
