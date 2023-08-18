7b4508fc537285bb9e99da70e0a1f5d2ed6a9d5b
Magnus Rand <magnus.rand@gmail.com>
2023-08-18 11:45:07 +0200

    test(datepicker): update tests after dependency update
    affects: @entur/datepicker

--- packages/datepicker/src/DatePicker/Datepicker.test.tsx
+++ packages/datepicker/src/DatePicker/Datepicker.test.tsx
@@ -29,184 +29,193 @@ expect.extend(toHaveNoViolations);
    In the mean time, locale="en-GB" is used, as this one is correctly applied and ensures stability no mater where the
    test is run */
 
-test('renders a DatePicker', () => {
-  const spy = jest.fn();
-  const currentDate = new CalendarDate(1997, 7, 10);
-  const { container, getByRole, queryAllByLabelText } = render(
-    <DatePicker
-      label="test"
-      selectedDate={currentDate}
-      onChange={spy}
-      locale="en-GB"
-    />,
-  );
+describe('DatePicker', () => {
+  test('is rendered', () => {
+    const spy = jest.fn();
+    const currentDate = new CalendarDate(1997, 7, 10);
+    const { container, getByRole, queryAllByLabelText } = render(
+      <DatePicker
+        label="test"
+        selectedDate={currentDate}
+        onChange={spy}
+        locale="en-GB"
+      />,
+    );
 
-  const openCalendarButton = container.getElementsByClassName(
-    'eds-datepicker__open-calendar-button',
-  )[0];
+    const openCalendarButton = container.getElementsByClassName(
+      'eds-datepicker__open-calendar-button',
+    )[0];
 
-  expect(getByRole('spinbutton', { name: 'test year' })).toHaveTextContent(
-    '1997',
-  );
-  expect(getByRole('spinbutton', { name: 'test month' })).toHaveTextContent(
-    '07',
-  );
-  expect(getByRole('spinbutton', { name: 'test day' })).toHaveTextContent('10');
-  expect(queryAllByLabelText('test')[0]).toBeInTheDocument();
-  expect(openCalendarButton).toBeInTheDocument();
-});
+    expect(getByRole('spinbutton', { name: 'year, test' })).toHaveTextContent(
+      '1997',
+    );
+    expect(getByRole('spinbutton', { name: 'month, test' })).toHaveTextContent(
+      '07',
+    );
+    expect(getByRole('spinbutton', { name: 'day, test' })).toHaveTextContent(
+      '10',
+    );
+    expect(queryAllByLabelText('test')[0]).toBeInTheDocument();
+    expect(openCalendarButton).toBeInTheDocument();
+  });
 
-test('correct time zone is applied', () => {
-  const spy = jest.fn();
-  const currentDate =
-    // should be 04:35 in Los Angeles
-    new ZonedDateTime(2022, 8, 25, 'America/Los_Angeles', -28800, 11, 35);
+  test('applies correct time zone', () => {
+    const spy = jest.fn();
+    const currentDate =
+      // should be 04:35 in Los Angeles
+      new ZonedDateTime(2022, 8, 25, 'America/Los_Angeles', -28800, 11, 35);
 
-  const { getByRole } = render(
-    <DatePicker
-      label="test"
-      selectedDate={currentDate}
-      onChange={spy}
-      locale="en-GB"
-      showTime
-    />,
-  );
+    const { getByRole } = render(
+      <DatePicker
+        label="test"
+        selectedDate={currentDate}
+        onChange={spy}
+        locale="en-GB"
+        showTime
+      />,
+    );
 
-  expect(getByRole('spinbutton', { name: 'test year' })).toHaveTextContent(
-    '2022',
-  );
-  expect(getByRole('spinbutton', { name: 'test month' })).toHaveTextContent(
-    '08',
-  );
-  expect(getByRole('spinbutton', { name: 'test day' })).toHaveTextContent('25');
-  expect(getByRole('spinbutton', { name: 'test hour' })).toHaveTextContent(
-    '04',
-  );
-  expect(getByRole('spinbutton', { name: 'test minute' })).toHaveTextContent(
-    '35',
-  );
-});
+    expect(getByRole('spinbutton', { name: 'year, test' })).toHaveTextContent(
+      '2022',
+    );
+    expect(getByRole('spinbutton', { name: 'month, test' })).toHaveTextContent(
+      '08',
+    );
+    expect(getByRole('spinbutton', { name: 'day, test' })).toHaveTextContent(
+      '25',
+    );
+    expect(getByRole('spinbutton', { name: 'hour, test' })).toHaveTextContent(
+      '04',
+    );
+    expect(getByRole('spinbutton', { name: 'minute, test' })).toHaveTextContent(
+      '35',
+    );
+  });
 
-test('locale is respected', () => {
-  const spy = jest.fn();
-  const currentDate = new CalendarDate(1997, 7, 10);
+  test('applies correct locale', () => {
+    const spy = jest.fn();
+    const currentDate = new CalendarDate(1997, 7, 10);
 
-  const { getByRole } = render(
-    <DatePicker
-      label="dutch"
-      selectedDate={currentDate}
-      onChange={spy}
-      locale="nl-NL"
-    />,
-  );
-  expect(getByRole('spinbutton', { name: 'dutch dag' })).toBeInTheDocument();
-  expect(getByRole('spinbutton', { name: 'dutch maand' })).toBeInTheDocument();
-  expect(getByRole('spinbutton', { name: 'dutch jaar' })).toBeInTheDocument();
-});
+    const { getByRole } = render(
+      <DatePicker
+        label="dutch"
+        selectedDate={currentDate}
+        onChange={spy}
+        locale="nl-NL"
+      />,
+    );
+    expect(getByRole('spinbutton', { name: 'dag, dutch' })).toBeInTheDocument();
+    expect(
+      getByRole('spinbutton', { name: 'maand, dutch' }),
+    ).toBeInTheDocument();
+    expect(
+      getByRole('spinbutton', { name: 'jaar, dutch' }),
+    ).toBeInTheDocument();
+  });
 
-test('Focus is set to selected date on calendar button click', () => {
-  const spy = jest.fn();
-  const currentDate = new CalendarDate(1997, 7, 10);
+  test('sets focus to selected date on calendar button click', async () => {
+    const user = userEvent.setup();
+    const spy = jest.fn();
+    const currentDate = new CalendarDate(1997, 7, 10);
 
-  const { container } = render(
-    <DatePicker
-      label="test"
-      selectedDate={currentDate}
-      onChange={spy}
-      locale="en-GB"
-    />,
-  );
+    const { container } = render(
+      <DatePicker
+        label="test"
+        selectedDate={currentDate}
+        onChange={spy}
+        locale="en-GB"
+      />,
+    );
 
-  const openCalendarButton = container.getElementsByClassName(
-    'eds-datepicker__open-calendar-button',
-  )[0];
+    const openCalendarButton = container.getElementsByClassName(
+      'eds-datepicker__open-calendar-button',
+    )[0];
 
-  fireEvent(
-    openCalendarButton,
-    new MouseEvent('click', {
-      bubbles: true,
-    }),
-  );
+    await user.click(openCalendarButton);
 
-  const selectedDateInCalendar = container.getElementsByClassName(
-    'eds-datepicker__calendar__grid__cell--selected',
-  )[0];
-  expect(selectedDateInCalendar).toHaveFocus();
-});
+    const selectedDateInCalendar = container.getElementsByClassName(
+      'eds-datepicker__calendar__grid__cell--selected',
+    )[0];
+    expect(selectedDateInCalendar).toHaveFocus();
+  });
 
-test('Focus lock is working on popover calendar', async () => {
-  const spy = jest.fn();
-  const user = userEvent.setup();
-  const currentDate = new CalendarDate(1997, 7, 10);
+  test('applies focus lock when popover calendar is active', async () => {
+    const spy = jest.fn();
+    const user = userEvent.setup();
+    const currentDate = new CalendarDate(1997, 7, 10);
 
-  const { container } = render(
-    <DatePicker
-      label="test"
-      selectedDate={currentDate}
-      onChange={spy}
-      locale="en-GB"
-    />,
-  );
+    const { container } = render(
+      <DatePicker
+        label="test"
+        selectedDate={currentDate}
+        onChange={spy}
+        locale="en-GB"
+      />,
+    );
 
-  const openCalendarButton = container.getElementsByClassName(
-    'eds-datepicker__open-calendar-button',
-  )[0];
+    const openCalendarButton = container.getElementsByClassName(
+      'eds-datepicker__open-calendar-button',
+    )[0];
 
-  await user.click(openCalendarButton);
+    await user.click(openCalendarButton);
 
-  await user.keyboard('{Tab}{Tab}{Tab}');
+    await user.keyboard('{Tab}{Tab}{Tab}');
 
-  const selectedDateInCalendar = container.getElementsByClassName(
-    'eds-datepicker__calendar__grid__cell--selected',
-  )[0];
-  expect(selectedDateInCalendar).toHaveFocus();
-}, 10000);
+    const selectedDateInCalendar = container.getElementsByClassName(
+      'eds-datepicker__calendar__grid__cell--selected',
+    )[0];
+    expect(selectedDateInCalendar).toHaveFocus();
+  }, 10000);
 
-test('Escape closes calendar popover', () => {
-  const spy = jest.fn();
-  const currentDate = new CalendarDate(1997, 7, 10);
+  test('closes calendar popover on escape key', () => {
+    const spy = jest.fn();
+    const currentDate = new CalendarDate(1997, 7, 10);
 
-  const { container } = render(
-    <DatePicker
-      label="test"
-      selectedDate={currentDate}
-      onChange={spy}
-      locale="en-GB"
-    />,
-  );
+    const { container } = render(
+      <DatePicker
+        label="test"
+        selectedDate={currentDate}
+        onChange={spy}
+        locale="en-GB"
+      />,
+    );
 
-  const openCalendarButton = container.getElementsByClassName(
-    'eds-datepicker__open-calendar-button',
-  )[0];
+    const openCalendarButton = container.getElementsByClassName(
+      'eds-datepicker__open-calendar-button',
+    )[0];
 
-  fireEvent(
-    openCalendarButton,
-    new MouseEvent('click', {
-      bubbles: true,
-    }),
-  );
+    fireEvent(
+      openCalendarButton,
+      new MouseEvent('click', {
+        bubbles: true,
+      }),
+    );
 
-  const selectedDateInCalendar = container.getElementsByClassName(
-    'eds-datepicker__calendar__grid__cell--selected',
-  )[0];
+    const selectedDateInCalendar = container.getElementsByClassName(
+      'eds-datepicker__calendar__grid__cell--selected',
+    )[0];
 
-  fireEvent.keyDown(selectedDateInCalendar, { key: 'Escape', code: 'Escape' });
-  expect(selectedDateInCalendar).not.toBeInTheDocument();
-});
+    fireEvent.keyDown(selectedDateInCalendar, {
+      key: 'Escape',
+      code: 'Escape',
+    });
+    expect(selectedDateInCalendar).not.toBeInTheDocument();
+  });
 
-test("Doesn't violate basic accessibility requirements", async () => {
-  const spy = jest.fn();
-  const currentDate = new CalendarDate(1997, 7, 10);
-  const { container } = render(
-    <DatePicker
-      label="test"
-      selectedDate={currentDate}
-      onChange={spy}
-      locale="en-GB"
-    />,
-  );
-  const results = await axe(container);
-  expect(results).toHaveNoViolations();
+  test("doesn't violate basic accessibility requirements", async () => {
+    const spy = jest.fn();
+    const currentDate = new CalendarDate(1997, 7, 10);
+    const { container } = render(
+      <DatePicker
+        label="test"
+        selectedDate={currentDate}
+        onChange={spy}
+        locale="en-GB"
+      />,
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
 });
 
 test('Timezones should always be UTC', () => {
