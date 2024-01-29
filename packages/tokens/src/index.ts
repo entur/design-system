5bc34e587b3ba966b9b5d98d3bf8cc9ac4f48b27
Magnus Rand <magnus.rand@gmail.com>
2024-01-29 17:34:04 +0100

    chore(tokens): add base color variables

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -3,6 +3,8 @@ export * from './primitive';
 // @ts-ignore
 export * from './semantic';
 // @ts-ignore
+export * from './base';
+// @ts-ignore
 export * from './componentColors';
 // @ts-ignore
 export * from './legacy-tokens';
