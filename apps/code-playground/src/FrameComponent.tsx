820f4f04612e10a712bcab7bdb5b37e35ae80bef
Magnus Rand <magnus.rand@gmail.com>
2024-02-06 09:57:47 +0100

    fix(code-playground): use contrast wrapper instead of class on root

--- apps/code-playground/src/FrameComponent.tsx
+++ apps/code-playground/src/FrameComponent.tsx
@@ -5,6 +5,7 @@ import { Switch } from '@entur/form';
 import { ToastProvider } from '@entur/alert';
 
 import { TestBench } from './TestBench';
+import { Contrast } from '@entur/layout';
 
 type FrameComponentProps = {
   children: React.ReactNode;
@@ -14,34 +15,38 @@ const FrameComponent = ({ children }: FrameComponentProps) => {
   const [contrast, setContrast] = React.useState(false);
   const [darkMode, setDarkMode] = React.useState(false);
 
-  React.useEffect(() => {
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
     document.documentElement.setAttribute(
       'data-color-mode',
-      darkMode ? 'dark' : 'light',
+      darkMode ? 'light' : 'dark',
     );
-    document.documentElement.setAttribute(
-      'class',
-      contrast ? 'eds-contrast' : '',
-    );
-  }, [darkMode, contrast]);
+  };
+  const toggleContrast = () => {
+    setContrast(!contrast);
+  };
+
+  const Element = contrast ? Contrast : 'div';
 
   return (
-    <ToastProvider>
-      <div style={{ paddingBottom: '1rem', display: 'flex', gap: '1rem' }}>
-        <Label>
-          Dark mode
-          <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
-        </Label>
-        <Label>
-          Kontrast
-          <Switch checked={contrast} onChange={() => setContrast(!contrast)} />
-        </Label>
-      </div>
-      <div style={{ margin: '2rem' }}>
-        <TestBench />
-      </div>
-      {children}
-    </ToastProvider>
+    <Element className="code-playground__wrapper">
+      <ToastProvider>
+        <div style={{ paddingBottom: '1rem', display: 'flex', gap: '1rem' }}>
+          <Label>
+            Dark mode
+            <Switch checked={darkMode} onChange={toggleDarkMode} />
+          </Label>
+          <Label>
+            Kontrast
+            <Switch checked={contrast} onChange={toggleContrast} />
+          </Label>
+        </div>
+        <div style={{ margin: '2rem' }}>
+          <TestBench />
+        </div>
+        {children}
+      </ToastProvider>
+    </Element>
   );
 };
 
