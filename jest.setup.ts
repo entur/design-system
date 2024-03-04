8515a73229f40a86a1154748979e46299096c3aa
Magnus Rand <magnus.rand@gmail.com>
2024-03-04 14:41:42 +0100

    chore(jest): mock warnAboutMissingStyles function from @entur/utils
    since Jest does not use CSS in its tests, we mock out the function to avoid non-interesting errors
    cluttering the output

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom/extend-expect';
+
+// We don't want warnings about missing styles in our jest tests
+jest.mock('@entur/utils', () => {
+  const original = jest.requireActual('@entur/utils');
+  return {
+    ...original,
+    warnAboutMissingStyles: jest.fn(),
+  };
+});
