c22a8db1efcb22651f736b3b22e81b225426e463
Cecilie Hansen Rørås <cecilie.hansen.roras@entur.org>
2024-03-22 08:06:53 +0100

    chore(website): initialize gatbsy 5 project

---
+++ documentation/gatsby-config.js
@@ -0,0 +1,63 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Entur designsystem`,
+    url: `https://design.entur.no/`,
+    siteUrl: `https://design.entur.no/`,
+    description: `Velkommen til Entur sitt designsystem. Her vil du finne informasjon om bruk og API-dokumentasjon for komponentene våre.`,
+    image: `/images/Enturlogo_Blue_RGB.png`,
+  },
+  trailingSlash: 'never',
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sass',
+    'gatsby-transformer-sharp',
+    'gatsby-remark-images',
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {},
+          },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'media',
+        path: './src/media/',
+      },
+      __key: 'media',
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+      },
+      __key: 'pages',
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: './src/content',
+      },
+      __key: 'content',
+    },
+  ],
+};
