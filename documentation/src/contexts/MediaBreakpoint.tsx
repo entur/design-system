c757298e358868e4e12528c8d0f52a200140549c
Cecilie Hansen Rørås <cecilie.hansen.roras@entur.org>
2024-03-22 08:06:53 +0100

    wip - gatbsy 5

---
+++ documentation/src/contexts/MediaBreakpoint.tsx
@@ -0,0 +1,9 @@
+import { createMedia } from '@artsy/fresnel';
+import { breakpoints } from '@entur/tokens';
+export const { MediaContextProvider, Media } = createMedia({
+  breakpoints: {
+    mobile: 0,
+    desktop: breakpoints.large,
+    wideDesktop: breakpoints.extraLarge,
+  },
+});
