841013a6d5c7ab72b62dbadc03b3d6ccf1ce29f5
Magnus Rand <magnus.rand@gmail.com>
2024-09-30 16:52:30 +0200

    fixup(playground): add all eds packages to scope. Fixup this commit into main commit

---
+++ documentation/src/components/Playground/packages-scope.ts
@@ -0,0 +1,47 @@
+import * as a11y from '@entur/a11y';
+import * as alert from '@entur/alert';
+import * as button from '@entur/button';
+import * as chip from '@entur/chip';
+import * as datepicker from '@entur/datepicker';
+import * as dropdown from '@entur/dropdown';
+import * as expand from '@entur/expand';
+import * as fileupload from '@entur/fileupload';
+import * as form from '@entur/form';
+import * as grid from '@entur/grid';
+import * as icons from '@entur/icons';
+import * as layout from '@entur/layout';
+import * as loader from '@entur/loader';
+import * as menu from '@entur/menu';
+import * as modal from '@entur/modal';
+import * as tab from '@entur/tab';
+import * as table from '@entur/table';
+import * as tokens from '@entur/tokens';
+import * as tooltip from '@entur/tooltip';
+import * as travel from '@entur/travel';
+import * as typography from '@entur/typography';
+import * as utils from '@entur/utils';
+
+export const packages = {
+  ...a11y,
+  ...alert,
+  ...button,
+  ...chip,
+  ...datepicker,
+  ...dropdown,
+  ...expand,
+  ...fileupload,
+  ...form,
+  ...grid,
+  ...icons,
+  ...layout,
+  ...loader,
+  ...menu,
+  ...modal,
+  ...tab,
+  ...table,
+  ...tokens,
+  ...tooltip,
+  ...travel,
+  ...typography,
+  ...utils,
+};
