2d07c3f14c1b59b7ecf00494646cd0ee6223cf05
Cecilie Hansen Rørås <cecilie.hansen.roras@entur.org>
2024-10-11 12:05:43 +0200

    wip playground

--- documentation/src/layouts/DocLayout.tsx
+++ documentation/src/layouts/DocLayout.tsx
@@ -7,6 +7,57 @@ import { MobileSideNavigation } from '../components/Navigations/SideNavigation/M
 import { TableOfContent } from '../components/Navigations/TableOfContent/TableOfContent';
 import { Media } from '../contexts/MediaBreakpoint';
 import TopNavigationLayout from './TopNavigationLayout';
+
+import { MDXProvider } from '@mdx-js/react';
+import { Playground } from '../components/Playground/Playground';
+
+const preToCodeBlock = preProps => {
+  console.log('preProps:', preProps); // Log the full preProps to inspect
+
+  if (
+    preProps.children &&
+    typeof preProps.children === 'object' &&
+    preProps.children.props &&
+    preProps.children.type === 'code'
+  ) {
+    const { children: codeString, props = {} } = preProps.children || {};
+
+    if (!codeString || typeof codeString !== 'string') {
+      console.warn('Unexpected structure:', preProps.children);
+      return undefined;
+    }
+
+    const { className } = props;
+
+    return {
+      codeString: codeString.trim(),
+      language: className ? className.split('-')[1] : '',
+      ...props,
+    };
+  }
+  return undefined;
+};
+
+const components = {
+  pre: preProps => {
+    console.log('preProps:', preProps); // Log the incoming preProps to inspect
+
+    const props = preToCodeBlock(preProps); // Call preToCodeBlock
+
+    console.log('Processed props:', props); // Log what preToCodeBlock returns
+
+    // Check if props were successfully extracted
+    if (props) {
+      console.log('Rendering Playground with props:', props);
+      return <Playground {...props} />;
+    } else {
+      // If no valid props, render the default <pre> block
+      console.log('Rendering default <pre> block:', preProps);
+      return <pre {...preProps} />;
+    }
+  },
+};
+
 interface LayoutProps {
   pageTitle: string;
   children: React.ReactNode;
@@ -56,7 +107,10 @@ const DocLayout: React.FC<LayoutProps> = ({
           'site-content--hidden': openSidebar,
         })}
       >
-        <main id="site-content">{children}</main>
+        <main id="site-content">
+          {' '}
+          <MDXProvider components={components}>{children}</MDXProvider>
+        </main>
         <TableOfContent />
       </div>
     </div>
