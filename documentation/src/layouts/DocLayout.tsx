7548d4aa230a9c7803b1cdc6fe85721febce9b91
Cecilie Hansen Rørås <cecilie.hansen.roras@entur.org>
2024-10-18 17:55:45 +0200

    fix(playground): add mdx-provider

--- documentation/src/layouts/DocLayout.tsx
+++ documentation/src/layouts/DocLayout.tsx
@@ -2,62 +2,13 @@ import * as React from 'react';
 import classNames from 'classnames';
 import { graphql, useStaticQuery } from 'gatsby';
 import { MenuItem } from '../components/Navigations/SideNavigation/utils';
-import { SideNavigation } from '../components/Navigations/SideNavigation/SideNavigation';
-import { MobileSideNavigation } from '../components/Navigations/SideNavigation/MobileSideNavigation';
-import { TableOfContent } from '../components/Navigations/TableOfContent/TableOfContent';
-import { Media } from '../contexts/MediaBreakpoint';
+import SideNavigation from '../components/Navigations/SideNavigation/SideNavigation';
+import MobileSideNavigation from '../components/Navigations/SideNavigation/MobileSideNavigation';
+import TableOfContent from '../components/Navigations/TableOfContent/TableOfContent';
+import { Media } from '../providers/MediaBreakpoint';
 import TopNavigationLayout from './TopNavigationLayout';
-
 import { MDXProvider } from '@mdx-js/react';
-import { Playground } from '../components/Playground/Playground';
-
-const preToCodeBlock = preProps => {
-  console.log('preProps:', preProps); // Log the full preProps to inspect
-
-  if (
-    preProps.children &&
-    typeof preProps.children === 'object' &&
-    preProps.children.props &&
-    preProps.children.type === 'code'
-  ) {
-    const { children: codeString, props = {} } = preProps.children || {};
-
-    if (!codeString || typeof codeString !== 'string') {
-      console.warn('Unexpected structure:', preProps.children);
-      return undefined;
-    }
-
-    const { className } = props;
-
-    return {
-      codeString: codeString.trim(),
-      language: className ? className.split('-')[1] : '',
-      ...props,
-    };
-  }
-  return undefined;
-};
-
-const components = {
-  pre: preProps => {
-    console.log('preProps:', preProps); // Log the incoming preProps to inspect
-
-    const props = preToCodeBlock(preProps); // Call preToCodeBlock
-
-    console.log('Processed props:', props); // Log what preToCodeBlock returns
-
-    // Check if props were successfully extracted
-    if (props) {
-      console.log('Rendering Playground with props:', props);
-      return <Playground {...props} />;
-    } else {
-      // If no valid props, render the default <pre> block
-      console.log('Rendering default <pre> block:', preProps);
-      return <pre {...preProps} />;
-    }
-  },
-};
-
+import components from './MdxProvider-utils';
 interface LayoutProps {
   pageTitle: string;
   children: React.ReactNode;
@@ -108,8 +59,7 @@ const DocLayout: React.FC<LayoutProps> = ({
         })}
       >
         <main id="site-content">
-          {' '}
-          <MDXProvider components={components}>{children}</MDXProvider>
+          <MDXProvider components={components}>{children} </MDXProvider>
         </main>
         <TableOfContent />
       </div>
