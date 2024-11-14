2ea891918eeb8e1f37e44093735e5abaff237a7a
Magnus Rand <magnus.rand@gmail.com>
2024-11-14 14:55:44 +0100

    refactor(illustration list): improve useGetIllustrations to return categorized data instead of query
    affects: @entur/documentation

--- apps/documentation/src/gatsby-theme-docz/components/useGetIllustrations.ts
+++ apps/documentation/src/gatsby-theme-docz/components/useGetIllustrations.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 
 export const useGetIllustrations = () => {
@@ -36,9 +37,15 @@ export const useGetIllustrations = () => {
       }
     }
   `);
-  return (query.pngFiles.illustrations as IllustrationsQueryType[]).concat(
-    query.otherFiles.illustrations as IllustrationsQueryType[],
-  );
+  const illustrationsQuery = (
+    query.pngFiles.illustrations as IllustrationsQueryType[]
+  ).concat(query.otherFiles.illustrations as IllustrationsQueryType[]);
+
+  const illustrations = useMemo(() => {
+    return processIllustrationsQuery(illustrationsQuery);
+  }, []);
+
+  return illustrations;
 };
 
 export type IllustrationsQueryType = {
@@ -50,3 +57,101 @@ export type IllustrationsQueryType = {
     fluid: any;
   };
 };
+
+export type IllustrationListItem = {
+  categories: string[];
+  name: string;
+  sanitizedName: string;
+  uniqueName: string;
+  fluidSource: any;
+  publicUrl: string;
+  publicUrls: Array<{ src: string; format: string }>;
+  publicUrlPNG?: string;
+  publicUrlSVG?: string;
+  isContrast: boolean;
+  isDarkmode: boolean;
+  hasBackground: boolean;
+  extension: string;
+};
+
+const ACCEPTED_EXTENSIONS = ['png', 'svg', 'pdf'];
+
+const processIllustrationsQuery = (
+  illustrationsQuery: IllustrationsQueryType[],
+) =>
+  illustrationsQuery
+    .filter(
+      illustration =>
+        ACCEPTED_EXTENSIONS.includes(illustration.extension.toLowerCase()) &&
+        illustration.absolutePath.includes('/downloads/illustrations/'),
+    )
+    // Process raw GraphQL-data into info about illustration
+    .map(illustration => {
+      const illustrationName = illustration.name.replace(/-|_/g, ' ');
+      const sanitizedName =
+        illustration.name.charAt(0) +
+        illustration.name
+          .toLowerCase()
+          .replace(' contrast', '')
+          .replace(' darkmode', '')
+          .replace(' default', '')
+          .replace(' circle', '')
+          .slice(1);
+      const categories = illustration.absolutePath
+        .split('/downloads/illustrations/')?.[1]
+        ?.split('/')
+        .filter(
+          category =>
+            // exclude file extension and file name as categories
+            !ACCEPTED_EXTENSIONS.includes(category.toLowerCase()) &&
+            !category.includes('.'),
+        )
+        .map(category => category.toLowerCase());
+      const publicUrl = illustration.publicURL;
+      const fluidSource = illustration.childImageSharp?.fluid;
+      const isContrast = illustrationName.toLowerCase().includes('contrast');
+      const isDarkmode = illustrationName
+        .toLowerCase()
+        .replace(/\s+|-|_/g, '')
+        .includes('darkmode');
+      const hasBackground =
+        illustrationName.toLowerCase().includes('circle') ||
+        illustrationName.toLowerCase().includes('frame') ||
+        illustrationName.toLowerCase().includes('background');
+
+      return {
+        name: illustrationName,
+        sanitizedName,
+        uniqueName: categories?.join('-') + illustrationName,
+        categories,
+        extension: illustration.extension,
+        publicUrl,
+        publicUrls: [],
+        fluidSource,
+        isContrast,
+        isDarkmode,
+        hasBackground,
+      } as IllustrationListItem;
+    })
+    // Merge svg and png version of the same illustration
+    .reduce((mergedIllustrationsList, illustration) => {
+      const matchedIllustration = mergedIllustrationsList.find(
+        illustrationInList =>
+          illustration.uniqueName === illustrationInList.uniqueName,
+      );
+
+      const publicUrl = {
+        src: illustration.publicUrl,
+        format: illustration.extension,
+      };
+      if (matchedIllustration) {
+        matchedIllustration.publicUrls.push(publicUrl);
+        if (matchedIllustration.fluidSource === undefined)
+          matchedIllustration.fluidSource = illustration.fluidSource;
+      } else {
+        illustration.publicUrls.push(publicUrl);
+        mergedIllustrationsList.push(illustration);
+      }
+
+      return mergedIllustrationsList;
+    }, [] as IllustrationListItem[]);
