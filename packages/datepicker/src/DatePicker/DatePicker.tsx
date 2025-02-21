78443c9568c4f8e82e26a7c65ffee9a164a21bf5
Magnus Rand <magnus.rand@gmail.com>
2025-02-21 15:36:45 +0100

    fix(datepicker): change default datetime type to ZonedDateTime
    This is done to avoid unexpected issues where a date without time is selected and sometimes leads to
    a crash.
    
    BREAKING CHANGE: ZonedDateTime is now returned from onChange if previous value was null.
    Previously return type was conditionned by component type.

--- packages/datepicker/src/DatePicker/DatePicker.tsx
+++ packages/datepicker/src/DatePicker/DatePicker.tsx
@@ -195,11 +195,11 @@ export const DatePicker = <DateType extends DateValue>({
   const { width } = useWindowDimensions();
 
   const handleOnChange = (value: MappedDateValue<DateType> | null) => {
-    if (forcedReturnType !== undefined) {
+    if (forcedReturnType !== undefined || !selectedDate) {
       return onChange(
         convertValueToType({
           value,
-          type: forcedReturnType,
+          type: forcedReturnType ?? 'ZonedDateTime',
           timezone:
             value !== null && 'timezone' in value
               ? (value.timezone as string)
@@ -272,7 +272,7 @@ export const DatePicker = <DateType extends DateValue>({
     weekNumberHeader,
   };
 
-  const useModal =
+  const isModal =
     typeof width !== 'undefined' &&
     width <= CALENDAR_MODAL_MAX_SCREEN_WIDTH &&
     !disableModal;
@@ -282,7 +282,7 @@ export const DatePicker = <DateType extends DateValue>({
       style={{ ...floatingStyles, zIndex: zIndexes.popover }}
       ref={refs.setFloating}
     >
-      <FocusLock disabled={!state.isOpen || useModal} returnFocus>
+      <FocusLock disabled={!state.isOpen || isModal} returnFocus>
         {state.isOpen && <Calendar {...calendarSharedProps} />}
       </FocusLock>
     </div>
@@ -352,7 +352,7 @@ export const DatePicker = <DateType extends DateValue>({
         validationVariant={validationVariant}
         variant={variant}
       />
-      {useModal ? modalCalendar : popoverCalendar}
+      {isModal ? modalCalendar : popoverCalendar}
     </ConditionalWrapper>
   );
 };
