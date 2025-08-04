bc46366788ae39edb4935472000c0af5f07a8dc8
Magnus Rand <magnus.rand@gmail.com>
2025-08-04 11:27:10 +0200

    fix(website): mobile sidenavigation broken after sidenavigation changes

--- apps/documentation/src/components/Navigations/SideNavigation/MobileSideNavigation.tsx
+++ apps/documentation/src/components/Navigations/SideNavigation/MobileSideNavigation.tsx
@@ -14,21 +14,15 @@ type MobileMenuProps = {
   menuItems: MenuItem[];
   openSidebar: boolean;
   setOpenSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+  currentLocation: Location;
 };
 
 const MobileSideNavigation: React.FC<MobileMenuProps> = ({
   menuItems,
   openSidebar,
   setOpenSidebar,
+  currentLocation,
 }) => {
-  useEffect(() => {
-    const siteContent = document.body;
-    if (!siteContent) return;
-
-    if (openSidebar) siteContent.style.overflow = 'hidden';
-    else siteContent.style.overflow = '';
-  }, [openSidebar]);
-
   useEffect(() => () => setOpenSidebar(false), [setOpenSidebar]);
 
   return (
@@ -42,14 +36,16 @@ const MobileSideNavigation: React.FC<MobileMenuProps> = ({
       >
         <SideNavigation
           menuItems={menuItems}
-          mobile={true}
           onClickMenuItem={() => setOpenSidebar(false)}
+          currentLocation={currentLocation}
           className="side-navigation__drawer__wrapper"
         />
       </Drawer>
       <FloatingButton
         size="medium"
-        className={classNames('side-navigation__drawer__menu-button')}
+        className={classNames('side-navigation__drawer__menu-button', {
+          'side-navigation__drawer__menu-button--open': openSidebar,
+        })}
         onClick={() => setOpenSidebar(!openSidebar)}
         type="button"
         aria-label={openSidebar ? 'Lukk meny' : 'Åpne meny'}
--- apps/documentation/src/layouts/SideNavigationLayout.tsx
+++ apps/documentation/src/layouts/SideNavigationLayout.tsx
@@ -58,6 +58,7 @@ const SideNavigationLayout = ({
         menuItems={menuItems}
         openSidebar={openSidebar}
         setOpenSidebar={setOpenSidebar}
+        currentLocation={location}
       />
     );
   if (isLargeScreen)
