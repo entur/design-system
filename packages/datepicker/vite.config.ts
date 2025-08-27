e8c8ab3ae1626f06867df6cd20576305227269d2
Magnus Rand <magnus.rand@gmail.com>
2025-08-27 15:16:27 +0200

    fix: exclude dependencies from bundle

--- packages/datepicker/vite.config.ts
+++ packages/datepicker/vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { createBaseConfig } from '../../vite.config.base';
 import { getPackageName, createFileNameFunction } from '../../vite.utils';
+import pkg from './package.json';
+import pkg from './package.json';
 
 export default defineConfig(() => {
   const packageName = getPackageName(__dirname);
@@ -14,6 +16,12 @@ export default defineConfig(() => {
       }),
     ],
     build: {
+      rollupOptions: {
+        external: [
+          ...Object.keys((pkg as any).dependencies || {}),
+          ...Object.keys((pkg as any).peerDependencies || {}),
+        ],
+      },
       lib: {
         entry: resolve(__dirname, 'src/index.tsx'),
         formats: ['es', 'cjs'],
