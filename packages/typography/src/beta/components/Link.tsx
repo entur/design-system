8579d0be41026763cbfc4c19f3afa5b5f780b708
Cecilie Hansen Rørås <cecilie.hansen.roras@entur.org>
2025-09-23 13:28:13 +0200

    feat(typography beta): add new special typography components
    Adds blockquote, link, list item, numbered list and unordered list. See
    linje.entur.no/komponenter/ressurser/typography-beta

---
+++ packages/typography/src/beta/components/Link.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import classNames from 'classnames';
+import { PolymorphicComponentProps } from '@entur/utils';
+import { ExternalIcon } from '@entur/icons';
+import { TypographySpacing } from '../types';
+import { getSpacingClasses } from '../utils/utils';
+
+import './text.scss';
+
+/**
+ * @beta Experimental component - may have breaking changes in future releases
+ */
+type LinkBaseProps = {
+  external?: boolean;
+
+  /** Ekstra klassenavn */
+  className?: string;
+  /** Innholdet */
+  children: React.ReactNode;
+  /** Spacing around the component (same as Text and Heading components) */
+  spacing?: TypographySpacing;
+  ariaLabelExternalIcon?: string;
+};
+
+export type LinkProps<C extends React.ElementType> = PolymorphicComponentProps<
+  C,
+  LinkBaseProps
+>;
+
+/**
+ * @beta Experimental component - may have breaking changes in future releases
+ */
+export const Link = <C extends React.ElementType = 'a'>({
+  external = false,
+  ariaLabelExternalIcon = '(ekstern lenke)',
+  className,
+  spacing,
+  children,
+  as,
+  ...rest
+}: LinkProps<C>): JSX.Element => {
+  const LinkElement: React.ElementType = as || 'a';
+  return (
+    <LinkElement
+      className={classNames(
+        'eds-text--link',
+        getSpacingClasses(spacing, 'eds-text--link'),
+        className,
+      )}
+      {...rest}
+    >
+      {children}
+      {external ? (
+        <ExternalIcon
+          className="eds-text--link--ext-icon"
+          aria-label={ariaLabelExternalIcon}
+        />
+      ) : null}
+    </LinkElement>
+  );
+};
