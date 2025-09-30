6a388fa5853fe611a086bc68f632436d6d04331a
Cecilie Hansen Rørås <cecilie.hansen.roras@entur.org>
2025-09-30 15:36:03 +0200

    fix(website): remove console.log from propstableresolver

--- apps/documentation/src/components/sanity/PropsTableResolver.tsx
+++ apps/documentation/src/components/sanity/PropsTableResolver.tsx
@@ -12,7 +12,5 @@ type Props = {
 
 export const PropsTableResolver = ({ value, npmPackage }: Props) => {
   const { componentName } = value;
-  console.log('PropsTableResolver npmPackage:', npmPackage);
-
   return <Props componentName={componentName} npmPackage={npmPackage} />;
 };
