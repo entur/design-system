4fe9374367d533cb6a76f368984083c163aac327
Magnus Rand <magnus.rand@gmail.com>
2025-11-28 14:24:32 +0100

    feat(typography/beta): change from dynamic css import to main entry styles file
    This is based on dynamic imports not providing enough value to outweigh its downsides
    
    BREAKING CHANGE: You must now add "@import '@entur/typography/beta/styles'" in you styles entry to
    get styling

--- packages/typography/src/beta/index.tsx
+++ packages/typography/src/beta/index.tsx
@@ -1,6 +1,8 @@
 // Beta typography components - experimental features
 // These components may have breaking changes in future releases
 
+import './index.scss';
+
 export { Heading } from './components/Heading';
 export { Text } from './components/Text';
 export { Link } from './components/Link';
