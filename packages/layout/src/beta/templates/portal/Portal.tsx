69d1f2a08d499d387745253eea29637ccdaa4b8a
Magnus Rand <magnus.rand@gmail.com>
2026-02-12 12:55:40 +0100

    feat(layout/beta): add support for `ref` and `as` props to Grid, Flex and Template components

--- packages/layout/src/beta/templates/portal/Portal.tsx
+++ packages/layout/src/beta/templates/portal/Portal.tsx
@@ -1,50 +1,71 @@
 import React from 'react';
+import type { PolymorphicComponentProps } from '@entur/utils';
 import classNames from 'classnames';
 import { Grid } from '../../Grid';
 import { Sidebar, SidebarComponent } from '../Sidebar';
 import './Portal.scss';
 
-export type PortalProps = React.HTMLAttributes<HTMLDivElement>;
-
-export type PortalMainProps = React.HTMLAttributes<HTMLElement>;
-
-const PortalRoot: React.FC<PortalProps> = ({
-  children,
-  className,
-  style,
-  ...rest
-}) => {
-  return (
-    <Grid
-      templateColumns="var(--eds-sidebar-width, min-content) minmax(0, 1fr)"
-      gap="m"
-      className={classNames('eds-layout-template-portal', className)}
-      style={style}
-      {...rest}
-    >
-      {children}
-    </Grid>
-  );
+type PortalOwnProps = {
+  className?: string;
+  style?: React.CSSProperties;
+  children?: React.ReactNode;
 };
 
-const PortalMain: React.FC<PortalMainProps> = ({
-  children,
-  className,
-  style,
-  ...rest
-}) => {
-  return (
-    <Grid.Item
-      as="main"
-      colSpan="2 / -1"
-      className={classNames('eds-layout-template-portal__main', className)}
-      style={style}
-      {...rest}
-    >
-      {children}
-    </Grid.Item>
-  );
+type PortalMainOwnProps = {
+  className?: string;
+  style?: React.CSSProperties;
+  children?: React.ReactNode;
 };
+
+const defaultPortalMainElement = 'main';
+
+export type PortalProps<T extends React.ElementType = typeof Grid> =
+  PolymorphicComponentProps<T, PortalOwnProps>;
+
+export type PortalMainProps<
+  T extends React.ElementType = typeof defaultPortalMainElement,
+> = PolymorphicComponentProps<T, PortalMainOwnProps>;
+
+const PortalRoot = React.forwardRef(
+  <E extends React.ElementType = typeof Grid>(
+    { children, className, style, as, ...rest }: PortalProps<E>,
+    ref?: React.Ref<Element>,
+  ) => {
+    return (
+      <Grid
+        ref={ref}
+        as={as}
+        templateColumns="var(--eds-sidebar-width, min-content) minmax(0, 1fr)"
+        gap="m"
+        className={classNames('eds-layout-template-portal', className)}
+        style={style}
+        {...rest}
+      >
+        {children}
+      </Grid>
+    );
+  },
+);
+
+const PortalMain = React.forwardRef(
+  <E extends React.ElementType = typeof defaultPortalMainElement>(
+    { children, className, style, as, ...rest }: PortalMainProps<E>,
+    ref?: React.Ref<Element>,
+  ) => {
+    return (
+      <Grid.Item
+        ref={ref}
+        as={as || defaultPortalMainElement}
+        colSpan="2 / -1"
+        className={classNames('eds-layout-template-portal__main', className)}
+        style={style}
+        {...rest}
+      >
+        {children}
+      </Grid.Item>
+    );
+  },
+);
 
 export type PortalComponent = typeof PortalRoot & {
   Sidebar: SidebarComponent;
