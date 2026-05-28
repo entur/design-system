7c5df0ad6841c0851aeb688092779448c0fb4956
Magnus Rand <magnus.rand@gmail.com>
2026-05-28 15:30:13 +0200

    feat(icons): add deprecation warnings for renamed partner icons
    Old flat Partner icon names (e.g. AKTIcon, VyIcon, BergenBysykkelIcon)
    now emit console.warn pointing consumers to the new structured names
    (e.g. AKTLogoMonoIcon, VyLogoIcon). Also adds deprecation entries for
    GOAColorIcon, ReisNordlandColorIcon, SJNordIcon, and BaneNORIcon which
    were renamed to match the new naming convention.
    
    Consumers can continue using the old names; they will see a warning
    in the console until they migrate to the new names.

--- packages/icons/bin/build.ts
+++ packages/icons/bin/build.ts
@@ -13,16 +13,298 @@ type Component = {
   name: string;
   svgPath: string;
   isDeprecated?: boolean;
-  replacement?: string;
+  deprecationMessage?: string;
   categories: string[];
 };
 
-/**
- * Deprecated icons, mapped to their possible replacements.
- * If an icon is deprecated without a replacement, it is mapped to no value,
- * and you can still check for deprecation using `deprecatedIcons.has(iconName)`.
- */
-const DEPRECATED_ICONS = new Map([['BellIcon', 'AlertIcon']]);
+type DeprecatedIcon = {
+  oldName: string;
+  replacementName?: string;
+  customMessage?: string;
+};
+
+const DEPRECATED_ICONS: DeprecatedIcon[] = [
+  { oldName: 'BellIcon', replacementName: 'AlertIcon' },
+  {
+    oldName: 'BrakarNoTextIcon',
+    replacementName: 'BrakarSymbolMonoIcon',
+    customMessage:
+      'BrakarNoTextIcon is deprecated; use BrakarSymbolMonoIcon for monochrome, BrakarSymbolIcon for color symbol, or BrakarLogoIcon for full logo',
+  },
+  {
+    oldName: 'TromsFylkestrafikkIcon',
+    replacementName: 'TromsLogoMonoIcon',
+    customMessage:
+      'TromsFylkestrafikkIcon is deprecated; use TromsLogoMonoIcon for monochrome or TromsLogoIcon for brand colors',
+  },
+  {
+    oldName: 'HafjellIcon',
+    replacementName: 'HafjellbussenLogoIcon',
+    customMessage:
+      'HafjellIcon is deprecated; use HafjellbussenLogoIcon for brand colors or HafjellbussenLogoMonoIcon for monochrome',
+  },
+  {
+    oldName: 'OttoMobilityIcon',
+    replacementName: 'OttoLogoIcon',
+    customMessage:
+      'OttoMobilityIcon is deprecated; use OttoLogoIcon for brand colors or OttoLogoMonoIcon for monochrome',
+  },
+  {
+    oldName: 'RuterNoTextIcon',
+    replacementName: 'RuterSymbolMonoIcon',
+    customMessage:
+      'RuterNoTextIcon is deprecated; use RuterSymbolMonoIcon for monochrome, RuterSymbolIcon for color symbol, or RuterLogoIcon for full logo',
+  },
+  {
+    oldName: 'SkyssNoTextIcon',
+    replacementName: 'SkyssSymbolMonoIcon',
+    customMessage:
+      'SkyssNoTextIcon is deprecated; use SkyssSymbolMonoIcon for monochrome, SkyssSymbolIcon for color symbol, or SkyssLogoIcon for full logo',
+  },
+  {
+    oldName: 'BaneNORIcon',
+    replacementName: 'BaneNORLogoMonoIcon',
+    customMessage:
+      'BaneNORIcon is deprecated; use BaneNORLogoMonoIcon for monochrome',
+  },
+  // Naming oddities fixed up
+  {
+    oldName: 'GOAColorIcon',
+    replacementName: 'GOALogoIcon',
+    customMessage:
+      'GOAColorIcon is deprecated; use GOALogoIcon for brand colors or GOALogoMonoIcon for monochrome',
+  },
+  {
+    oldName: 'ReisNordlandIcon',
+    replacementName: 'ReisNordlandLogoMonoIcon',
+    customMessage:
+      'ReisNordlandIcon is deprecated; use ReisNordlandLogoMonoIcon for monochrome or ReisNordlandLogoIcon for brand colors',
+  },
+  {
+    oldName: 'ReisNordlandColorIcon',
+    replacementName: 'ReisNordlandLogoIcon',
+    customMessage:
+      'ReisNordlandColorIcon is deprecated; use ReisNordlandLogoIcon for brand colors or ReisNordlandLogoMonoIcon for monochrome',
+  },
+  {
+    oldName: 'SJNordIcon',
+    replacementName: 'SJNordLogoMonoIcon',
+    customMessage:
+      'SJNordIcon is deprecated; use SJNordLogoMonoIcon for monochrome',
+  },
+  // Old root Partner/ icons (were currentColor logos, now split into LogoMono/Logo)
+  {
+    oldName: 'AKTIcon',
+    replacementName: 'AKTLogoMonoIcon',
+    customMessage:
+      'AKTIcon is deprecated; use AKTLogoMonoIcon for monochrome or AKTLogoIcon for brand colors',
+  },
+  {
+    oldName: 'AlesundTurvognIcon',
+    replacementName: 'AlesundTurvognLogoMonoIcon',
+    customMessage:
+      'AlesundTurvognIcon is deprecated; use AlesundTurvognLogoMonoIcon for monochrome or AlesundTurvognLogoIcon for brand colors',
+  },
+  {
+    oldName: 'AtBIcon',
+    replacementName: 'AtBLogoMonoIcon',
+    customMessage:
+      'AtBIcon is deprecated; use AtBLogoMonoIcon for monochrome or AtBLogoIcon for brand colors',
+  },
+  {
+    oldName: 'BrakarIcon',
+    replacementName: 'BrakarLogoMonoIcon',
+    customMessage:
+      'BrakarIcon is deprecated; use BrakarLogoMonoIcon for monochrome or BrakarLogoIcon for brand colors',
+  },
+  {
+    oldName: 'FarteIcon',
+    replacementName: 'FarteLogoMonoIcon',
+    customMessage:
+      'FarteIcon is deprecated; use FarteLogoMonoIcon for monochrome or FarteLogoIcon for brand colors',
+  },
+  {
+    oldName: 'FlybussenIcon',
+    replacementName: 'FlybussenLogoMonoIcon',
+    customMessage:
+      'FlybussenIcon is deprecated; use FlybussenLogoMonoIcon for monochrome or FlybussenLogoIcon for brand colors',
+  },
+  {
+    oldName: 'FramIcon',
+    replacementName: 'FramLogoMonoIcon',
+    customMessage:
+      'FramIcon is deprecated; use FramLogoMonoIcon for monochrome or FramLogoIcon for brand colors',
+  },
+  {
+    oldName: 'GOAIcon',
+    replacementName: 'GOALogoMonoIcon',
+    customMessage:
+      'GOAIcon is deprecated; use GOALogoMonoIcon for monochrome or GOALogoIcon for brand colors',
+  },
+  {
+    oldName: 'HykeIcon',
+    replacementName: 'HykeLogoMonoIcon',
+    customMessage:
+      'HykeIcon is deprecated; use HykeLogoMonoIcon for monochrome or HykeLogoIcon for brand colors',
+  },
+  {
+    oldName: 'InnlandstrafikkIcon',
+    replacementName: 'InnlandstrafikkLogoMonoIcon',
+    customMessage:
+      'InnlandstrafikkIcon is deprecated; use InnlandstrafikkLogoMonoIcon for monochrome or InnlandstrafikkLogoIcon for brand colors',
+  },
+  {
+    oldName: 'KolumbusIcon',
+    replacementName: 'KolumbusLogoMonoIcon',
+    customMessage:
+      'KolumbusIcon is deprecated; use KolumbusLogoMonoIcon for monochrome or KolumbusLogoIcon for brand colors',
+  },
+  {
+    oldName: 'NordlandIcon',
+    replacementName: 'NordlandLogoMonoIcon',
+    customMessage:
+      'NordlandIcon is deprecated; use NordlandLogoMonoIcon for monochrome or NordlandLogoIcon for brand colors',
+  },
+  {
+    oldName: 'OstfoldIcon',
+    replacementName: 'OstfoldLogoMonoIcon',
+    customMessage:
+      'OstfoldIcon is deprecated; use OstfoldLogoMonoIcon for monochrome or OstfoldLogoIcon for brand colors',
+  },
+  {
+    oldName: 'RuterIcon',
+    replacementName: 'RuterLogoMonoIcon',
+    customMessage:
+      'RuterIcon is deprecated; use RuterLogoMonoIcon for monochrome or RuterLogoIcon for brand colors',
+  },
+  {
+    oldName: 'SJIcon',
+    replacementName: 'SJLogoMonoIcon',
+    customMessage:
+      'SJIcon is deprecated; use SJLogoMonoIcon for monochrome or SJLogoIcon for brand colors',
+  },
+  {
+    oldName: 'SkyssIcon',
+    replacementName: 'SkyssLogoMonoIcon',
+    customMessage:
+      'SkyssIcon is deprecated; use SkyssLogoMonoIcon for monochrome or SkyssLogoIcon for brand colors',
+  },
+  {
+    oldName: 'SnelandiaIcon',
+    replacementName: 'SnelandiaLogoMonoIcon',
+    customMessage:
+      'SnelandiaIcon is deprecated; use SnelandiaLogoMonoIcon for monochrome or SnelandiaLogoIcon for brand colors',
+  },
+  {
+    oldName: 'SvipperIcon',
+    replacementName: 'SvipperLogoMonoIcon',
+    customMessage:
+      'SvipperIcon is deprecated; use SvipperLogoMonoIcon for monochrome or SvipperLogoIcon for brand colors',
+  },
+  {
+    oldName: 'VKTIcon',
+    replacementName: 'VKTLogoMonoIcon',
+    customMessage:
+      'VKTIcon is deprecated; use VKTLogoMonoIcon for monochrome or VKTLogoIcon for brand colors',
+  },
+  {
+    oldName: 'VyIcon',
+    replacementName: 'VyLogoMonoIcon',
+    customMessage:
+      'VyIcon is deprecated; use VyLogoMonoIcon for monochrome or VyLogoIcon for brand colors',
+  },
+  {
+    oldName: 'ØresundstågIcon',
+    replacementName: 'OresundstagLogoMonoIcon',
+    customMessage:
+      'ØresundstågIcon is deprecated; use OresundstagLogoMonoIcon for monochrome or OresundstagLogoIcon for brand colors',
+  },
+  // Old Mobility/ icons (moved to Logo/)
+  {
+    oldName: 'BergenBysykkelIcon',
+    replacementName: 'BergenBysykkelLogoMonoIcon',
+    customMessage:
+      'BergenBysykkelIcon is deprecated; use BergenBysykkelLogoMonoIcon for monochrome or BergenBysykkelLogoIcon for brand colors',
+  },
+  {
+    oldName: 'BoltIcon',
+    replacementName: 'BoltLogoMonoIcon',
+    customMessage:
+      'BoltIcon is deprecated; use BoltLogoMonoIcon for monochrome or BoltLogoIcon for brand colors',
+  },
+  {
+    oldName: 'FarteBysykkelIcon',
+    replacementName: 'FarteBysykkelLogoMonoIcon',
+    customMessage:
+      'FarteBysykkelIcon is deprecated; use FarteBysykkelLogoMonoIcon for monochrome or FarteBysykkelLogoIcon for brand colors',
+  },
+  {
+    oldName: 'GetAroundIcon',
+    replacementName: 'GetAroundLogoMonoIcon',
+    customMessage:
+      'GetAroundIcon is deprecated; use GetAroundLogoMonoIcon for monochrome or GetAroundLogoIcon for brand colors',
+  },
+  {
+    oldName: 'HertzIcon',
+    replacementName: 'HertzLogoMonoIcon',
+    customMessage:
+      'HertzIcon is deprecated; use HertzLogoMonoIcon for monochrome or HertzLogoIcon for brand colors',
+  },
+  {
+    oldName: 'KolumbusBysykkelIcon',
+    replacementName: 'KolumbusBysykkelLogoMonoIcon',
+    customMessage:
+      'KolumbusBysykkelIcon is deprecated; use KolumbusBysykkelLogoMonoIcon for monochrome or KolumbusBysykkelLogoIcon for brand colors',
+  },
+  {
+    oldName: 'LimeIcon',
+    replacementName: 'LimeLogoMonoIcon',
+    customMessage:
+      'LimeIcon is deprecated; use LimeLogoMonoIcon for monochrome or LimeLogoIcon for brand colors',
+  },
+  {
+    oldName: 'MoveAboutIcon',
+    replacementName: 'MoveAboutLogoMonoIcon',
+    customMessage:
+      'MoveAboutIcon is deprecated; use MoveAboutLogoMonoIcon for monochrome or MoveAboutLogoIcon for brand colors',
+  },
+  {
+    oldName: 'OsloBysykkelIcon',
+    replacementName: 'OsloBysykkelLogoMonoIcon',
+    customMessage:
+      'OsloBysykkelIcon is deprecated; use OsloBysykkelLogoMonoIcon for monochrome or OsloBysykkelLogoIcon for brand colors',
+  },
+  {
+    oldName: 'SURFIcon',
+    replacementName: 'SurfLogoMonoIcon',
+    customMessage:
+      'SURFIcon is deprecated; use SurfLogoMonoIcon for monochrome or SurfLogoIcon for brand colors',
+  },
+  {
+    oldName: 'TierIcon',
+    replacementName: 'TierLogoMonoIcon',
+    customMessage:
+      'TierIcon is deprecated; use TierLogoMonoIcon for monochrome or TierLogoIcon for brand colors',
+  },
+  {
+    oldName: 'TrondheimBysykkelIcon',
+    replacementName: 'TrondheimBysykkelLogoMonoIcon',
+    customMessage:
+      'TrondheimBysykkelIcon is deprecated; use TrondheimBysykkelLogoMonoIcon for monochrome or TrondheimBysykkelLogoIcon for brand colors',
+  },
+  {
+    oldName: 'VoiIcon',
+    replacementName: 'VoiLogoMonoIcon',
+    customMessage:
+      'VoiIcon is deprecated; use VoiLogoMonoIcon for monochrome or VoiLogoIcon for brand colors',
+  },
+  {
+    oldName: 'ZvippIcon',
+    replacementName: 'ZvippLogoMonoIcon',
+    customMessage:
+      'ZvippIcon is deprecated; use ZvippLogoMonoIcon for monochrome or ZvippLogoIcon for brand colors',
+  },
+];
 
 // These icons should not use brand-blue by default
 const OUTLIER_CATEGORIES = [
@@ -37,14 +319,12 @@ const SPECIAL_OUTLIERS = ['CompassNeedleIcon'];
 buildIconComponents();
 
 async function buildIconComponents() {
-  const components = traverse('src/svgs').map((svgPath: string) => {
-    // Check for .DS_Store to clarify confusing error message
+  const components = traverse('src/svgs').flatMap((svgPath: string) => {
+    // Remove .DS_Store files automatically
     if (svgPath.endsWith('.DS_Store')) {
-      console.error(
-        '\nWARNING: You have a .DS_Store file among your svgs, please remove it. Path:',
-        svgPath,
-        '\n',
-      );
+      fs.removeSync(svgPath);
+      console.log('Removed .DS_Store file:', svgPath);
+      return [];
     }
     const name = getComponentNameFromSvgPath(svgPath);
     const categories = svgPath
@@ -53,13 +333,17 @@ async function buildIconComponents() {
       .filter(
         (category: string) => category !== '' && !category.includes('.svg'),
       );
-    const isDeprecated = DEPRECATED_ICONS.has(name);
-    const replacement = DEPRECATED_ICONS.get(name);
+    const deprecationInfo = DEPRECATED_ICONS.find(d => d.oldName === name);
+    const isDeprecated = deprecationInfo !== undefined;
+    const deprecationMessage = isDeprecated
+      ? deprecationInfo.customMessage ??
+        getDeprecationMessage(name, deprecationInfo.replacementName)
+      : undefined;
     return {
       name,
       svgPath,
       isDeprecated,
-      replacement,
+      deprecationMessage,
       categories,
     } as Component;
   });
@@ -104,20 +388,17 @@ async function outputComponentCode({
 /** Add deprecation warnings if an icon is deprecated */
 function addDeprecationWarnings(
   webCode: string,
-  { isDeprecated, name, replacement }: Component,
+  { isDeprecated, deprecationMessage }: Component,
 ) {
-  if (isDeprecated) {
+  if (isDeprecated && deprecationMessage) {
     const webCodeList = webCode.split(`\n`);
 
     const functionDeclarationLine = webCodeList.findIndex(line =>
       /^function/.test(line),
     );
 
-    const deprecationMessage = getDeprecationMessage(name, replacement);
     const jsdocInsertionPoint = functionDeclarationLine;
-    const numberOfLinesThatTheFunctionDeclarationTakesUp = 1;
-    const consoleLogInsertionPoint =
-      functionDeclarationLine + numberOfLinesThatTheFunctionDeclarationTakesUp;
+    const consoleLogInsertionPoint = functionDeclarationLine + 1;
 
     return [
       ...webCodeList.slice(0, jsdocInsertionPoint),
@@ -133,12 +414,13 @@ function addDeprecationWarnings(
 function createTypeDeclaration(components: Component[]) {
   const typingsPreamble = fs.readFileSync('./types/index.d.ts').toString();
   const componentTypeLines = components.flatMap(
-    ({ name, isDeprecated, replacement }) => {
+    ({ name, isDeprecated, deprecationMessage }) => {
       const typeDeclaration = `export declare const ${name}: React.FC<IconProps & React.SVGProps<SVGElement>>;`;
-      if (isDeprecated) {
-        const deprecationMessage = getDeprecationMessage(name, replacement);
-        const jsdocComment = createDeprecatedJsdocComment(deprecationMessage);
-        return [jsdocComment, typeDeclaration];
+      if (isDeprecated && deprecationMessage) {
+        return [
+          createDeprecatedJsdocComment(deprecationMessage),
+          typeDeclaration,
+        ];
       }
       return typeDeclaration;
     },
