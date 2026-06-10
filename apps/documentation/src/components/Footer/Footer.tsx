eee5e9cd7201283562099ad781d3525ba2fe577a
Magnus Rand <magnus.rand@gmail.com>
2026-06-10 16:10:16 +0200

    fix(website): simplify footer markup and use semantic HTML
    Remove contrast mode and extra wrapper elements. Replace custom grid
    divs with native CSS Grid. Add border matching top nav divider.
    Footer now renders as a direct <footer> element in the grid.

--- apps/documentation/src/components/Footer/Footer.tsx
+++ apps/documentation/src/components/Footer/Footer.tsx
@@ -1,17 +1,19 @@
 import React, { RefObject } from 'react';
-import { Link } from 'gatsby';
 import classNames from 'classnames';
-import { IconButton } from '@entur/button';
-import { FacebookIcon, InstagramIcon, LinkedinIcon } from '@entur/icons';
-import { Link as DSLink, Heading3 } from '@entur/typography';
+import {
+  CookieFilledIcon,
+  EmailIcon,
+  FigmaIcon,
+  GithubIcon,
+} from '@entur/icons';
+import { Link, Heading3 } from '@entur/typography';
 
 import { Logo } from '@components/Logo/Logo';
-import { Theme, useSettings } from '@providers/SettingsContext';
+import { useSettings } from '@providers/SettingsContext';
 
 import './Footer.scss';
 
 const Footer = ({
-  forceColorMode,
   footerRef,
   className,
   contrast,
@@ -29,83 +31,59 @@ const Footer = ({
     >
       <div className="footer__grid-container">
         <div>
-          <Logo colorMode={contrast ? "dark" : colorMode} />
+          <Logo colorMode={contrast ? 'dark' : colorMode} />
           <p className="footer__description">
             Entur Linje er designsystemet til Entur. Det hjelper designere og
             utviklere å bygge konsistente digitale tjenester.
           </p>
-          <div className="footer__social">
-            <IconButton
-              as="a"
-              href="https://www.facebook.com/entur.org/"
-              aria-label="Entur på Facebook (ekstern lenke)"
-            >
-              <FacebookIcon size="24" />
-            </IconButton>
-            <IconButton
-              as="a"
-              href="https://www.instagram.com/entur_as/"
-              aria-label="Entur på Instagram (ekstern lenke)"
-            >
-              <InstagramIcon size="24" />
-            </IconButton>
-            <IconButton
-              as="a"
-              href="https://www.linkedin.com/company/entur-as"
-              aria-label="Entur på LinkedIn (ekstern lenke)"
-            >
-              <LinkedinIcon size="24" />
-            </IconButton>
-          </div>
         </div>
         <nav aria-label="Om nettstedet">
           <Heading3 margin="bottom">Om nettstedet</Heading3>
-          <DSLink as={Link} to="/kom-i-gang" className="footer__link">
-            Kom i gang
-          </DSLink>
-          <DSLink as={Link} to="/identitet" className="footer__link">
-            Identitet
-          </DSLink>
-          <DSLink as={Link} to="/komponenter" className="footer__link">
-            Komponenter
-          </DSLink>
-          <DSLink as={Link} to="/tokens" className="footer__link">
-            Tokens
-          </DSLink>
-          <DSLink as={Link} to="/universell-utforming" className="footer__link">
-            Universell utforming
-          </DSLink>
-          <DSLink
+          <Link
             href="https://uustatus.no/nb/erklaringer/publisert/7c5b8f79-7c24-4144-8084-afde897edded"
             className="footer__link"
           >
             Tilgjengelighetserklæring
-          </DSLink>
+          </Link>
+          <Link
+            as="button"
+            type="button"
+            onClick={() => window.__ucCmp.showFirstLayer()}
+            className="footer__link"
+          >
+            <CookieFilledIcon inline aria-hidden="true" /> Endre hvilken
+            informasjon vi får lagre
+          </Link>
         </nav>
         <div>
           <Heading3 margin="bottom">Kom i kontakt med oss</Heading3>
-          <DSLink
+          <Link
             href="https://entur.slack.com/archives/C899QSPB7"
             className="footer__link"
           >
             #talk-designsystem på Slack
-          </DSLink>
-          <DSLink
+          </Link>
+          <Link
             href="https://github.com/entur/design-system"
             className="footer__link"
           >
-            GitHub
-          </DSLink>
-          <DSLink
+            <GithubIcon inline aria-hidden="true" /> GitHub
+          </Link>
+          <Link
+            href="https://www.figma.com/files/911324992315480847/team/903289363381269328"
+            className="footer__link"
+          >
+            <FigmaIcon inline aria-hidden="true" /> Figma (krever innlogging)
+          </Link>
+          <Link
             href="mailto:teamdesignsystem@entur.org"
             className="footer__link"
           >
-            teamdesignsystem@entur.org
-          </DSLink>
+            <EmailIcon inline aria-hidden="true" /> teamdesignsystem@entur.org
+          </Link>
         </div>
       </div>
       <div className="footer__entur-banner">
-        <span className="eds-label footer__entur-banner__item">Entur.no</span>
         <span className="eds-label">© {new Date().getFullYear()} Entur AS</span>
       </div>
     </footer>
