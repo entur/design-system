3a6fd3a4d468d144dc7555febb3ee506bafc9028
Magnus Rand <magnus.rand@gmail.com>
2026-06-26 13:01:06 +0200

    refactor(datepicker): replace useRandomId with React useId

--- packages/datepicker/src/DatePicker/CalendarGrid.tsx
+++ packages/datepicker/src/DatePicker/CalendarGrid.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import { useId } from 'react';
 
 import { useLocale } from '@react-aria/i18n';
 import { useCalendarGrid } from '@react-aria/calendar';
 import { CalendarState, RangeCalendarState } from '@react-stately/calendar';
 import { CalendarDate, getWeeksInMonth } from '@internationalized/date';
 
-import { useRandomId } from '@entur/utils';
 import { VisuallyHidden } from '@entur/a11y';
 
 import { getWeekNumberForDate } from '../shared/utils';
@@ -32,7 +31,7 @@ export const CalendarGrid = ({
   weekNumberHeader,
   renderCell,
 }: CalendarGridProps) => {
-  const calendarGridId = useRandomId('eds-calendar');
+  const calendarGridId = `eds-calendar${useId()}`;
   const { locale } = useLocale();
 
   const gridStartDate = startDate ?? state.visibleRange.start;
