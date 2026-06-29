15dbaa4e3636e33b8a3e698326593e25d8d266c9
Magnus Rand <magnus.rand@gmail.com>
2026-06-29 15:26:43 +0200

    fix(utils): deprecate useRandomId in favor of React useId

--- packages/utils/src/useRandomId.ts
+++ packages/utils/src/useRandomId.ts
@@ -1,5 +1,16 @@
 import { useId } from 'react';
 
+/**
+ * @deprecated Use `useId()` from React directly instead.
+ * Will be removed in a future major version.
+ *
+ * ```diff
+ * - import { useRandomId } from '@entur/utils';
+ * - const id = useRandomId('eds-my-component');
+ * + import { useId } from 'react';
+ * + const id = `eds-my-component${useId()}`;
+ * ```
+ */
 export const useRandomId = (prefix?: string): string => {
   const id = useId();
   return prefix ? `${prefix}${id}` : id;
